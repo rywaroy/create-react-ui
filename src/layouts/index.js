6bd2ab5f8d5f154d6689b9e4a0ff5a50139d8d16
张志豪 <464485682@qq.com>
2019-11-26 15:53:50 +0800

    Feat: layouts

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,15 +1,31 @@
-import styles from './index.css';
 import React, { Component } from 'react';
+import { Layout } from 'antd';
+import MenuBox from '@/components/MenuBox';
+
+const { Content, Sider } = Layout;
 
 class BasicLayout extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { };
-    }
 
     render() {
         return (
-            <div>111</div>
+            <Layout style={{ height: '100%', minWidth: 1200 }}>
+                <Layout>
+                    <Sider width={200} style={{ background: '#fff' }}>
+                        <MenuBox />
+                    </Sider>
+                    <Layout style={{ padding: '0 24px 24px', background: '#23232e' }}>
+                        <Content
+                            style={{
+                                padding: 24,
+                                margin: 0,
+                                minHeight: 280,
+                            }}
+                        >
+                            {this.props.children}
+                        </Content>
+                    </Layout>
+                </Layout>
+            </Layout>
         );
     }
 }
