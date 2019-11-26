5b33adb7ff1846103ea7536ab4db818595e97e48
张志豪 <464485682@qq.com>
2019-11-26 15:54:15 +0800

    Feat: config

--- .umirc.js
+++ .umirc.js
@@ -1,5 +1,6 @@
-
 // ref: https://umijs.org/config/
+const path = require('path'); 
+
 export default {
   treeShaking: true,
   routes: [
@@ -7,28 +8,40 @@ export default {
       path: '/',
       component: '../layouts/index',
       routes: [
-        { path: '/', component: '../pages/index' }
-      ]
-    }
+        {
+          path: '/',
+          component: '../pages/template',
+        },
+      ],
+    },
   ],
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
-    ['umi-plugin-react', {
-      antd: true,
-      dva: true,
-      dynamicImport: { webpackChunkName: true },
-      title: 'crui',
-      dll: true,
-      
-      routes: {
-        exclude: [
-          /models\//,
-          /services\//,
-          /model\.(t|j)sx?$/,
-          /service\.(t|j)sx?$/,
-          /components\//,
-        ],
+    [
+      'umi-plugin-react',
+      {
+        antd: true,
+        dva: true,
+        dynamicImport: {
+          webpackChunkName: true,
+        },
+        title: 'crui',
+        dll: true,
+        routes: {
+          exclude: [
+            /models\//,
+            /services\//,
+            /model\.(t|j)sx?$/,
+            /service\.(t|j)sx?$/,
+            /components\//,
+          ],
+        },
       },
-    }],
+    ],
   ],
-}
+  history: 'browser',
+  alias: {
+    '@': path.resolve(__dirname, 'src'),
+  },
+};
+
