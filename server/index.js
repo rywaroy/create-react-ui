717bfbd4232fade3ce748b09412e1affd060f859
张志豪 <464485682@qq.com>
2019-11-27 15:08:27 +0800

    feat: ws link

--- server/index.js
+++ server/index.js
@@ -3,8 +3,12 @@ const Koa = require('koa');
 const app = new Koa();
 const server = require('http').createServer(app.callback());
 const io = require('socket.io')(server);
-io.on('connection', () => {
+const file = require('./file');
 
+io.on('connection', (socket) => {
+
+    // 文件相关
+    file(socket);
 });
 
 server.listen(2019);
