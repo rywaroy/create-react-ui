0ec1f8a7ac07ab144e26a9722e0c41a6702fc830
张志豪 <464485682@qq.com>
2019-11-28 15:35:51 +0800

    Feat: 添加路由栈属性

--- server/file/display-files.js
+++ server/file/display-files.js
@@ -11,44 +11,57 @@ module.exports = function displayFiles(filePaths) {
         {
             title: '/',
             key: 1,
+            value: '',
         }
     ];
     const foldersArray = [
         {
             title: '/',
             key: 1,
+            value: '',
         }
     ];
+    const stack = [''];
     let key = 2;
-    function fileDisplayDeep(filePath, filesArray, foldersArray) {
+    function fileDisplayDeep(filePath, filesArray, foldersArray, isFirst) {
         const files = fs.readdirSync(filePath);
+        if (!isFirst) {
+            stack.push(path.basename(filePath)); // 添加路由栈
+        }
         files.forEach(filename => {
+            const urlStack = JSON.parse(JSON.stringify(stack)); // 深拷贝路由栈
+            const url = urlStack.join('/');
             const filedir = path.join(filePath, filename);
             const stats = fs.statSync(filedir);
             if (stats.isFile() && !/^\..*$/.test(filename)) { // 文件
                 filesArray.push({
                     title: filename,
+                    vaule: url,
                     key: key++,
                 });
             }
             if (stats.isDirectory() && !ignoreFile[filename]) { // 文件夹
                 const childrenFiles = [];
                 const childrenFolders = [];
+                key = key + 1;
                 filesArray.push({
                     title: filename,
-                    key: key++,
+                    key,
+                    value: url,
                     children: childrenFiles,
                 });
                 foldersArray.push({
                     title: filename,
-                    key: key++,
+                    key,
+                    value: url,
                     children: childrenFolders,
                 });
                 fileDisplayDeep(filedir, childrenFiles, childrenFolders);
             }
         });
+        stack.pop();
     }
-    fileDisplayDeep(filePaths, filesArray, foldersArray);
+    fileDisplayDeep(filePaths, filesArray, foldersArray, true);
     return {
         filesArray,
         foldersArray,
