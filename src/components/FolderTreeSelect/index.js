eab50fef2f04fcc00d9388ab7cfe8140a7643765
张志豪 <464485682@qq.com>
2019-11-29 10:31:44 +0800

    Feat: 文件路径选择组件

---
+++ src/components/FolderTreeSelect/index.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { TreeSelect } from 'antd';
+
+const { TreeNode } = TreeSelect;
+
+class FolderTreeSelect extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
+    onChange = value => {
+        this.setState({
+            value
+        });
+        this.props.onChange && this.props.onChange(value);
+    }
+
+    render() {
+        return (
+            <TreeSelect
+                showSearch
+                style={{ width: '100%' }}
+                value={this.state.value}
+                dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
+                placeholder="请选择路径"
+                allowClear
+                treeDefaultExpandAll
+                onChange={this.onChange}
+            >
+                {
+                    this.props.folders.map(folder => (
+                        <TreeNode value={folder.value} title={folder.title} key={folder.key}>
+                            {
+                                folder.children &&
+                                    folder.children.map(child => (
+                                        <TreeNode value={child.value} title={child.title} key={child.key}></TreeNode>
+                                    ))
+                            }
+                        </TreeNode>
+                    ))
+                }
+            </TreeSelect>
+        );
+    }
+}
+
+export default FolderTreeSelect;
