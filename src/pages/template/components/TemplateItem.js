484cdd700a176f19ce8eaf25aba13df473eda4a3
张志豪 <464485682@qq.com>
2019-11-29 10:15:12 +0800

    Feat: 更新为函数组件

--- src/pages/template/components/TemplateItem.js
+++ src/pages/template/components/TemplateItem.js
@@ -1,27 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Button } from 'antd';
 import styles from '../index.less';
 
-class TemplateItem extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { };
-    }
-
-    render() {
-        const { title, intro, imgClassName } = this.props;
-        return (
-            <li className={styles.item}>
-                <div className={`${styles.img} ${imgClassName}`}>
-                    <div className={styles.mask}>
-                        <Button type="primary">添加到项目</Button>
-                    </div>
+function TemplateItem(props) {
+    const { title, intro, imgClassName } = props;
+    return (
+        <div className={styles.item}>
+            <div className={`${styles.img} ${imgClassName}`}>
+                <div className={styles.mask}>
+                    <Button type="primary" onClick={() => props.add()}>添加到项目</Button>
                 </div>
-                <h3 className={styles.title}>{title}</h3>
-                <p className={styles.intro}>{intro}</p>
-            </li>
-        );
-    }
+            </div>
+            <h3 className={styles.title}>{title}</h3>
+            <p className={styles.intro}>{intro}</p>
+        </div>
+    );
 }
 
 export default TemplateItem;
