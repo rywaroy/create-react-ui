998854c22b15eef9da86d33d8948a01b7717542f
张志豪 <464485682@qq.com>
2019-11-29 14:31:41 +0800

    feat: 添加表单

--- src/pages/template/components/DefaultTemplate.js
+++ src/pages/template/components/DefaultTemplate.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Modal, Form } from 'antd';
+import { Modal, Form, Input } from 'antd';
 import TemplateItem from './TemplateItem';
+import FolderTreeSelect from '@/components/FolderTreeSelect';
 
 class DefaultTemplate extends Component {
     constructor(props) {
@@ -16,10 +17,28 @@ class DefaultTemplate extends Component {
             visible: true,
             key: Math.random(),
         });
+        this.props.form.setFields({
+            url: {
+                value: '',
+            },
+            folderName: {
+                value: '',
+            },
+            fileName: {
+                value: 'index.js',
+            },
+            variable: {
+                value: '',
+            },
+        });
     }
 
     handleOk = () => {
-
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
     }
 
     handleCancel = () => {
@@ -30,6 +49,8 @@ class DefaultTemplate extends Component {
 
     render() {
         const { visible, key } = this.state;
+        const { folders } = this.props;
+        const { getFieldDecorator } = this.props.form;
 
         return (
             <div>
@@ -44,11 +65,58 @@ class DefaultTemplate extends Component {
                     visible={visible}
                     onOk={this.handleOk}
                     onCancel={this.handleCancel}>
-
+                    <Form>
+                        <Form.Item label="添加到">
+                            {
+                                getFieldDecorator('url')(<FolderTreeSelect folders={folders}/>)
+                            }
+                        </Form.Item>
+                        <Form.Item label="文件夹名">
+                            {
+                                getFieldDecorator('folderName', {
+                                    rules: [
+                                        {
+                                            pattern: /^((?!\\|\/|\:|\?|\*|\"|<|>|\^).)*$/,
+                                            message: '请填写正确文件夹名',
+                                        },
+                                    ],
+                                })(<Input />)
+                            }
+                        </Form.Item>
+                        <Form.Item label="文件名">
+                            {
+                                getFieldDecorator('fileName', {
+                                    initialValue: 'index.js',
+                                    rules: [
+                                        {
+                                            required: true,
+                                            message: '请填写文件名',
+                                        },
+                                        {
+                                            pattern: /^[a-zA-Z0-9\-\_]+\.[a-zA-Z0-9\-\_]+$/,
+                                            message: '请填写正确文件名',
+                                        },
+                                    ],
+                                })(<Input />)
+                            }
+                        </Form.Item>
+                        <Form.Item label="变量名">
+                            {
+                                getFieldDecorator('variable', {
+                                    rules: [
+                                        {
+                                            pattern: /^[a-zA-Z\$_][a-zA-Z\d_]*$/,
+                                            message: '请填写正确变量名',
+                                        },
+                                    ],
+                                })(<Input />)
+                            }
+                        </Form.Item>
+                    </Form>
                 </Modal>
             </div>
         );
     }
 }
 
-export default DefaultTemplate;
+export default Form.create()(DefaultTemplate);
