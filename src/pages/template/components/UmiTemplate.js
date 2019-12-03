70c7516dea0af090d8c110e4473de5464712415f
张志豪 <464485682@qq.com>
2019-12-03 09:29:32 +0800

    feat: umi模板组件

---
+++ src/pages/template/components/UmiTemplate.js
@@ -0,0 +1,142 @@
+import React, { Component } from 'react';
+import { Modal, Form, Input, Radio } from 'antd';
+import TemplateItem from './TemplateItem';
+import FolderTreeSelect from '@/components/FolderTreeSelect';
+
+class UmiTemplate extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            visible: false,
+        };
+    }
+
+    addUmiTemplate = () => {
+        this.setState({
+            visible: true,
+        });
+        this.props.form.setFields({
+            folderName: {
+                value: '',
+            },
+            fileName: {
+                value: 'index.js',
+            },
+            variable: {
+                value: '',
+            },
+            namespace: {
+                value: '',
+            },
+        });
+    }
+
+    handleOk = () => {
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                window.socket.emit('create-umi-template', values);
+                this.handleCancel();
+            }
+        });
+    }
+
+    handleCancel = () => {
+        this.setState({
+            visible: false,
+        });
+    }
+
+    render() {
+        const { visible } = this.state;
+        const { folders } = this.props;
+        const { getFieldDecorator } = this.props.form;
+
+        return (
+            <div>
+                <TemplateItem
+                    title="umi模板"
+                    intro="包含index.js model.js 可配置变量名"
+                    imgClassName="umiImg"
+                    add={this.addUmiTemplate}/>
+                <Modal
+                    title="添加umi模板"
+                    visible={visible}
+                    onOk={this.handleOk}
+                    onCancel={this.handleCancel}>
+                    <Form>
+                        <Form.Item label="添加到">
+                            {
+                                getFieldDecorator('url')(<FolderTreeSelect folders={folders}/>)
+                            }
+                        </Form.Item>
+                        <Form.Item label="文件夹名">
+                            {
+                                getFieldDecorator('folderName', {
+                                    rules: [
+                                        {
+                                            pattern: /^((?!\\|\/|\:|\?|\*|\"|<|>|\^).)*$/,
+                                            message: '请填写正确文件夹名',
+                                        },
+                                    ],
+                                })(<Input />)
+                            }
+                        </Form.Item>
+                        <Form.Item label="文件名">
+                            {
+                                getFieldDecorator('fileName', {
+                                    initialValue: 'index.js',
+                                    rules: [
+                                        {
+                                            required: true,
+                                            message: '请填写文件名',
+                                        },
+                                        {
+                                            pattern: /^[a-zA-Z0-9\-\_]+\.[a-zA-Z0-9\-\_]+$/,
+                                            message: '请填写正确文件名',
+                                        },
+                                    ],
+                                })(<Input />)
+                            }
+                        </Form.Item>
+                        <Form.Item label="变量名">
+                            {
+                                getFieldDecorator('variable', {
+                                    rules: [
+                                        {
+                                            pattern: /^[a-zA-Z\$_][a-zA-Z\d_]*$/,
+                                            message: '请填写正确变量名',
+                                        },
+                                    ],
+                                })(<Input />)
+                            }
+                        </Form.Item>
+                        <Form.Item label="model namespace">
+                            {
+                                getFieldDecorator('namespace', {
+                                    rules: [
+                                        {
+                                            pattern: /^[a-zA-Z\$_][a-zA-Z\d_]*$/,
+                                            message: '请填写正确变量名',
+                                        },
+                                    ],
+                                })(<Input />)
+                            }
+                        </Form.Item>
+                        <Form.Item label="油涟自定义配置">
+                            {
+                                getFieldDecorator('oilConfig', {
+                                    initialValue: false
+                                })(<Radio.Group>
+                                    <Radio value={false}>否</Radio>
+                                    <Radio value={true}>是</Radio>
+                                </Radio.Group>)
+                            }
+                        </Form.Item>
+                    </Form>
+                </Modal>
+            </div>
+        );
+    }
+}
+
+export default Form.create()(UmiTemplate);
