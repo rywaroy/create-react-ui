ae983324055c56f2941b2f1a4c47a1ff18894e22
张志豪 <464485682@qq.com>
2019-12-05 17:11:07 +0800

    feat: 监听创建umi模板创建

--- server/template/index.js
+++ server/template/index.js
@@ -1,4 +1,5 @@
 const createDefaultTemplate = require('./create-default-template');
+const createUmiTemplate = require('./create-umi-template');
 const displayFiles = require('../file/display-files');
 
 module.exports = function template(socket) {
@@ -7,6 +8,23 @@ module.exports = function template(socket) {
     socket.on('create-default-template', async data => {
         try {
             await createDefaultTemplate(data);
+            socket.emit('msg', {
+                state: 1,
+                msg: '创建成功',
+            });
+            updateFiles(socket);
+        } catch (err) {
+            socket.emit('msg', {
+                state: 0,
+                msg: err,
+            });
+        }
+    });
+
+    // 创建umi模板
+    socket.on('create-umi-template', async data => {
+        try {
+            await createUmiTemplate(data);
             socket.emit('msg', {
                 state: 1,
                 msg: '创建成功',
