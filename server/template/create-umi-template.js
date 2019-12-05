84652d56bfbf2f7e2e8ead9aa80af6167d507476
张志豪 <464485682@qq.com>
2019-12-05 17:23:03 +0800

    feat: 创建umi model文件

--- server/template/create-umi-template.js
+++ server/template/create-umi-template.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const fs = require('fs');
 const execa = require('execa');
+const umiModel = require('../templateString/umi-model');
 const umiTemplate = require('../templateString/umi-template');
 
 module.exports = function createUmiTemplate({ url, folderName, fileName, variable, namespace, oilConfig }) {
     return new Promise(function (resolve, reject) {
         let base = path.join(process.cwd(), url ? url : '');
         variable = variable ? variable : 'Template';
+        namespace = namespace ? namespace : 'global';
 
         // 创建文件夹
         if (folderName) {
@@ -17,8 +19,10 @@ module.exports = function createUmiTemplate({ url, folderName, fileName, variabl
             execa.commandSync(`mkdir ${base}`);
         }
         const script = umiTemplate(variable, namespace);
+        const modelscript = umiModel(namespace, oilConfig);
 
         fs.writeFileSync(path.join(base, fileName), script);
+        fs.writeFileSync(path.join(base, 'model.js'), modelscript);
         resolve();
     });
 };
