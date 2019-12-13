3026877bdf55d0c449e07a1e1c1de10145fb4a12
张志豪 <464485682@qq.com>
2019-12-13 16:52:44 +0800

    feat: 创建自定义模板，创建指定文件夹功能

--- server/socket/template/create-custom-template.js
+++ server/socket/template/create-custom-template.js
@@ -1,5 +1,17 @@
+const path = require('path');
+const fs = require('fs');
+const execa = require('execa');
+
 module.exports = function createCustomTemplate({ url, folderName, fileName, variable }) {
     return new Promise(function (resolve, reject) {
-
+        let base = path.join(process.cwd(), url ? url : '');
+        // 创建文件夹
+        if (folderName) {
+            base = path.join(base, folderName);
+            if (fs.existsSync(base)) {
+                reject('该文件夹已存在');
+            }
+            execa.commandSync(`mkdir ${base}`);
+        }
     });
 };
