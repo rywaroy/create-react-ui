49db0b2e0dc6ae3e3326ca8500622e70b43d53fe
张志豪 <464485682@qq.com>
2019-12-17 15:41:33 +0800

    Feat: 导出表达式解析

--- server/socket/template/create-custom-template.js
+++ server/socket/template/create-custom-template.js
@@ -80,6 +80,34 @@ function createExportVisitor(ast, variable) {
                 path.node.declaration.name = variable;
                 traverse(ast, createVisitor(ast, variable, identifier));
             }
+
+            if (path.node.declaration.type === 'CallExpression') {
+
+                /**
+                 * 导出表达式
+                 * @example
+                 * class Component extends react.Component {}
+                 * export default connect(({ global }) => ({ global }))(Component);
+                 * // or
+                 * export default Form.create()(Component);
+                 * // or
+                 * export default connect(({ global }) => ({ global }))(Form.create()(Component));
+                 */
+
+                if (path.node.declaration.arguments.length > 0) {
+                    const argument = path.node.declaration.arguments[0];
+                    let identifier;
+                    if (argument.type === 'Identifier') {
+                        identifier = argument.name;
+                        argument.name = variable;
+                    }
+                    if (argument.type === 'CallExpression') {
+                        identifier = argument.arguments[0].name;
+                        argument.arguments[0].name = variable;
+                    }
+                    traverse(ast, createVisitor(ast, variable, identifier));
+                }
+            }
         },
 
     };
