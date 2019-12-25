e602869d88715b85d00e12f8cbf8eddfcd935c8d
张志豪 <464485682@qq.com>
2019-12-25 09:56:34 +0800

    feat: 添加导出文件名

--- src/pages/code/components/TableCode.js
+++ src/pages/code/components/TableCode.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
+import { Modal, Form, Input, Icon, Tooltip } from 'antd';
 import TemplateItem from '@/components/TemplateItem';
+import FolderTreeSelect from '@/components/FolderTreeSelect';
 
 class TableCode extends Component {
     constructor(props) {
         super(props);
-        this.state = { };
+        this.state = {
+            configVisible: false
+        };
     }
 
     addTableCode = () => {
-
+        this.setState({
+            configVisible: true
+        });
     }
 
     render() {
+        const { configVisible } = this.state;
+        const { files } = this.props;
+        const { getFieldDecorator } = this.props.form;
+
         return (
             <div>
                 <TemplateItem
@@ -19,9 +29,27 @@ class TableCode extends Component {
                     intro=""
                     imgClassName="customImg"
                     add={this.addTableCode}/>
+                <Modal
+                    title="table组件配置"
+                    visible={configVisible}>
+                    <Form>
+                        <Form.Item label="导出文件">
+                            {
+                                getFieldDecorator('url', {
+                                    rules: [
+                                        {
+                                            required: true,
+                                            message: '请填写导出文件',
+                                        },
+                                    ]
+                                })(<FolderTreeSelect folders={files}/>)
+                            }
+                        </Form.Item>
+                    </Form>
+                </Modal>
             </div>
         );
     }
 }
 
-export default TableCode;
+export default Form.create()(TableCode);
