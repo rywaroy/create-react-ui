bcab044d59fa459c3c40514dab244678b7386a5a
张志豪 <464485682@qq.com>
2019-12-27 09:28:25 +0800

    fix

--- server/socket/file/display-files.js
+++ server/socket/file/display-files.js
@@ -4,7 +4,7 @@ const path = require('path');
 const ignoreFile = {
     'node_modules': true,
     '.git': true,
-    '.crui': true,
+    '.crui': false,
 };
 
 module.exports = function displayFiles(filePaths) {
