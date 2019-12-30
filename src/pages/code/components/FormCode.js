4d6c3d04bca378997ca5993c579747b923b1caae
张志豪 <464485682@qq.com>
2019-12-30 14:58:17 +0800

    Feat: form code 组件配置弹窗ui

--- src/pages/code/components/FormCode.js
+++ src/pages/code/components/FormCode.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Modal, Form, Input, Icon, Button, message, TreeSelect } from 'antd';
 import TemplateItem from '@/components/TemplateItem';
 
 class FormCode extends Component {
@@ -7,6 +8,7 @@ class FormCode extends Component {
         this.state = {
             configVisible: false,
             configKey: Math.random(),
+            code: '',
         };
     }
 
@@ -33,6 +35,9 @@ class FormCode extends Component {
     }
 
     render() {
+        const { configVisible, configKey, code } = this.state;
+        const { files } = this.props;
+        const { getFieldDecorator } = this.props.form;
         return (
             <div>
                 <TemplateItem
@@ -40,9 +45,47 @@ class FormCode extends Component {
                     intro="配合油涟组件GenerateForm使用"
                     imgClassName="formImg"
                     add={this.openFormCode}/>
+                <Modal
+                    title="form组件配置"
+                    key={configKey}
+                    visible={configVisible}>
+                    <Form>
+                        <Form.Item label="导出文件">
+                            {
+                                getFieldDecorator('url', {
+                                    rules: [
+                                        {
+                                            required: true,
+                                            message: '请填写导出文件',
+                                        },
+                                    ]
+                                })(<TreeSelect
+                                    showSearch
+                                    style={{ width: '100%' }}
+                                    dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
+                                    placeholder="请选择路径"
+                                    allowClear
+                                    treeData={files}/>)
+                            }
+                        </Form.Item>
+                        <Form.Item label={<span>代码片段 <button style={{ background: 'none', border: '0', outline: 'none' }} data-clipboard-text={code} id="code"><Icon type="copy"/></button></span>}>
+                            {
+                                getFieldDecorator('code', {
+                                    rules: [
+                                        {
+                                            required: true,
+                                            message: '请先生成代码片段',
+                                        },
+                                    ]
+                                })(<Input disabled/>)
+                            }
+                        </Form.Item>
+                        <Button type="primary" onClick={this.openCreateCode}>代码生成</Button>
+                    </Form>
+                </Modal>
             </div>
         );
     }
 }
 
-export default FormCode;
+export default Form.create()(FormCode);
