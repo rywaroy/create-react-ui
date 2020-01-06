aae2c48d0f4dce43c96ee67bd1475669859144b8
张志豪 <464485682@qq.com>
2020-01-06 17:05:59 +0800

    feat: 添加统一的复制事件

--- src/pages/code/index.js
+++ src/pages/code/index.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'dva';
+import { message } from 'antd';
 import TableCode from './components/TableCode';
 import FormCode from './components/FormCode';
+import Clipboard from 'clipboard';
 
 class Code extends Component {
+
+    componentDidMount() {
+        this.clipboard = new Clipboard('#code');
+        this.clipboard.on('success', () => {
+            message.success('复制成功');
+        });
+        this.clipboard.on('error', () => {
+            message.error('复制失败');
+        });
+    }
+
+    componentWillUnmount() {
+        this.clipboard.destroy();
+    }
 
     render() {
         const { files } = this.props.global;
