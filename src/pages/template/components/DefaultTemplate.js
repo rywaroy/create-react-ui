623ede01a832c81e041355076e1b33076fa5cc32
张志豪 <464485682@qq.com>
2020-01-09 15:53:58 +0800

    Refactor: 默认react模板使用hooks

--- src/pages/template/components/DefaultTemplate.js
+++ src/pages/template/components/DefaultTemplate.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Modal, Form, Input, TreeSelect } from 'antd';
 import TemplateItem from '@/components/TemplateItem';
 
-class DefaultTemplate extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            visible: false,
-        };
-    }
+function DefaultTemplate(props) {
 
-    addDefaultTemplate = () => {
-        this.setState({
-            visible: true,
-        });
-        this.props.form.setFields({
+    const [ visible, setVisible ] = useState(false);
+    const { folders } = props;
+    const { getFieldDecorator } = props.form;
+
+    const addDefaultTemplate = () => {
+        setVisible(true);
+        props.form.setFields({
             folderName: {
                 value: '',
             },
@@ -25,100 +21,92 @@ class DefaultTemplate extends Component {
                 value: '',
             },
         });
-    }
+    };
 
-    handleOk = () => {
-        this.props.form.validateFields((err, values) => {
+    const handleOk = () => {
+        props.form.validateFields((err, values) => {
             if (!err) {
                 window.socket.emit('create-default-template', values);
-                this.handleCancel();
+                handleCancel();
             }
         });
-    }
+    };
 
-    handleCancel = () => {
-        this.setState({
-            visible: false,
-        });
-    }
+    const handleCancel = () => {
+        setVisible(false);
+    };
 
-    render() {
-        const { visible } = this.state;
-        const { folders } = this.props;
-        const { getFieldDecorator } = this.props.form;
-
-        return (
-            <div>
-                <TemplateItem
-                    title="默认react模板"
-                    intro="包含index.js 可配置变量名"
-                    imgClassName="defaultImg"
-                    add={this.addDefaultTemplate}/>
-                <Modal
-                    title="添加默认模板"
-                    visible={visible}
-                    onOk={this.handleOk}
-                    onCancel={this.handleCancel}>
-                    <Form>
-                        <Form.Item label="添加到">
-                            {
-                                getFieldDecorator('url')(
-                                    <TreeSelect
-                                        showSearch
-                                        style={{ width: '100%' }}
-                                        dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
-                                        placeholder="请选择路径"
-                                        allowClear
-                                        treeData={folders}/>
-                                )
-                            }
-                        </Form.Item>
-                        <Form.Item label="文件夹名">
-                            {
-                                getFieldDecorator('folderName', {
-                                    rules: [
-                                        {
-                                            pattern: /^((?!\\|\/|\:|\?|\*|\"|<|>|\^).)*$/,
-                                            message: '请填写正确文件夹名',
-                                        },
-                                    ],
-                                })(<Input />)
-                            }
-                        </Form.Item>
-                        <Form.Item label="文件名">
-                            {
-                                getFieldDecorator('fileName', {
-                                    initialValue: 'index.js',
-                                    rules: [
-                                        {
-                                            required: true,
-                                            message: '请填写文件名',
-                                        },
-                                        {
-                                            pattern: /^[a-zA-Z0-9\-\_]+\.[a-zA-Z0-9\-\_]+$/,
-                                            message: '请填写正确文件名',
-                                        },
-                                    ],
-                                })(<Input />)
-                            }
-                        </Form.Item>
-                        <Form.Item label="变量名">
-                            {
-                                getFieldDecorator('variable', {
-                                    rules: [
-                                        {
-                                            pattern: /^[a-zA-Z\$_][a-zA-Z\d_]*$/,
-                                            message: '请填写正确变量名',
-                                        },
-                                    ],
-                                })(<Input />)
-                            }
-                        </Form.Item>
-                    </Form>
-                </Modal>
-            </div>
-        );
-    }
+    return (
+        <div>
+            <TemplateItem
+                title="默认react模板"
+                intro="包含index.js 可配置变量名"
+                imgClassName="defaultImg"
+                add={addDefaultTemplate}/>
+            <Modal
+                title="添加默认模板"
+                visible={visible}
+                onOk={handleOk}
+                onCancel={handleCancel}>
+                <Form>
+                    <Form.Item label="添加到">
+                        {
+                            getFieldDecorator('url')(
+                                <TreeSelect
+                                    showSearch
+                                    style={{ width: '100%' }}
+                                    dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
+                                    placeholder="请选择路径"
+                                    allowClear
+                                    treeData={folders}/>
+                            )
+                        }
+                    </Form.Item>
+                    <Form.Item label="文件夹名">
+                        {
+                            getFieldDecorator('folderName', {
+                                rules: [
+                                    {
+                                        pattern: /^((?!\\|\/|\:|\?|\*|\"|<|>|\^).)*$/,
+                                        message: '请填写正确文件夹名',
+                                    },
+                                ],
+                            })(<Input />)
+                        }
+                    </Form.Item>
+                    <Form.Item label="文件名">
+                        {
+                            getFieldDecorator('fileName', {
+                                initialValue: 'index.js',
+                                rules: [
+                                    {
+                                        required: true,
+                                        message: '请填写文件名',
+                                    },
+                                    {
+                                        pattern: /^[a-zA-Z0-9\-\_]+\.[a-zA-Z0-9\-\_]+$/,
+                                        message: '请填写正确文件名',
+                                    },
+                                ],
+                            })(<Input />)
+                        }
+                    </Form.Item>
+                    <Form.Item label="变量名">
+                        {
+                            getFieldDecorator('variable', {
+                                rules: [
+                                    {
+                                        pattern: /^[a-zA-Z\$_][a-zA-Z\d_]*$/,
+                                        message: '请填写正确变量名',
+                                    },
+                                ],
+                            })(<Input />)
+                        }
+                    </Form.Item>
+                </Form>
+            </Modal>
+        </div>
+    );
 }
 
 export default Form.create()(DefaultTemplate);
