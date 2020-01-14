a860b917cc79b73a2e0696cfb1894a093af12393
张志豪 <464485682@qq.com>
2020-01-14 15:34:18 +0800

    refactor: 提取file操作接口

--- server/router/index.js
+++ server/router/index.js
@@ -1,35 +1,11 @@
 const Router = require('koa-router');
 const fs = require('fs');
 const path = require('path');
+const file = require('./file');
 
 const router = new Router();
 
-/**
- * 获取.crui/template下模板文件的目录
- */
-router.get('/api/file/template', async ctx => {
-    const base = path.join(process.cwd(), '.crui', 'template');
-    if (fs.existsSync(base)) {
-        const file = fs.readdirSync(base);
-        if (file.length === 0) {
-            ctx.error(0, '该目录下没有文件', null);
-        } else {
-            const data = [];
-            file.forEach((item, index) => {
-                if (/^.+\..+$/.test(item)) { // 判断是文件
-                    data.push({
-                        title: item,
-                        key: index + 1,
-                        value: item,
-                    });
-                }
-            });
-            ctx.success(200, '', data);
-        }
-    } else {
-        ctx.error(0, '找不到/.crui/template文件目录', null);
-    }
-});
+router.use('/api/file', file.routes(), file.allowedMethods());
 
 /**
  * 判断是否是js文件
