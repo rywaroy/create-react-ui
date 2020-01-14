2d786653274ed38362a411f7593aa31f526edc91
张志豪 <464485682@qq.com>
2020-01-14 15:36:26 +0800

    Refactor: 添加验证js文件接口

--- server/router/file.js
+++ server/router/file.js
@@ -31,4 +31,25 @@ router.get('/api/file/template', async ctx => {
     }
 });
 
+/**
+ * 判断是否是js文件
+ */
+router.get('/api/file/isjs', async ctx => {
+    const base = path.join(process.cwd(), ctx.query.url ? ctx.query.url : '');
+    if (fs.existsSync(base)) {
+        const stat = fs.statSync(base);
+        if (stat.isFile()) {
+            if (path.extname(base) === '.js') {
+                ctx.success(200, '验证成功', null);
+            } else {
+                ctx.error(-1, '不是js文件', null);
+            }
+        } else {
+            ctx.error(-1, '不是文件', null);
+        }
+    } else {
+        ctx.error(-1, '找不到该文件', null);
+    }
+});
+
 module.exports = router;
