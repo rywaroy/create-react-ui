bf3c5ed2ee2e361c0b5530d0ba6627a1e6753802
张志豪 <464485682@qq.com>
2020-01-19 09:52:54 +0800

    refactor: 💡 更新使用功能emit提示

--- server/socket/template/index.js
+++ server/socket/template/index.js
@@ -2,33 +2,18 @@ const createDefaultTemplate = require('./create-default-template');
 const createUmiTemplate = require('./create-umi-template');
 const createCoustomTemplate = require('./create-custom-template');
 const displayFiles = require('../file/display-files');
+const { createSuccessMsg, createFailMsg } = require('../common');
 
 module.exports = function template(socket) {
-
-    // 成功创建文件
-    function createSuccess() {
-        socket.emit('msg', {
-            status: 200,
-            msg: '创建成功',
-        });
-    }
-
-    // 创建失败
-    function createFail(err) {
-        socket.emit('msg', {
-            status: 0,
-            msg: err.message ? err.message : err,
-        });
-    }
 
     // 创建默认模板
     socket.on('create-default-template', async data => {
         try {
             await createDefaultTemplate(data);
-            createSuccess();
+            createSuccessMsg(socket);
             updateFiles(socket);
         } catch (err) {
-            createFail(err);
+            createFailMsg(socket, err);
         }
     });
 
@@ -36,10 +21,10 @@ module.exports = function template(socket) {
     socket.on('create-umi-template', async data => {
         try {
             await createUmiTemplate(data);
-            createSuccess();
+            createSuccessMsg(socket);
             updateFiles(socket);
         } catch (err) {
-            createFail(err);
+            createFailMsg(socket, err);
         }
     });
 
@@ -47,10 +32,10 @@ module.exports = function template(socket) {
     socket.on('create-custom-template', async data => {
         try {
             await createCoustomTemplate(data);
-            createSuccess();
+            createSuccessMsg();
             updateFiles(socket);
         } catch (err) {
-            createFail(err);
+            createFailMsg(socket, err);
         }
     });
 };
