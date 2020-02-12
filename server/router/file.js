9c447de7567f471e094be51e3c080384e447c6f4
张志豪 <464485682@qq.com>
2020-02-12 17:16:24 +0800

    refactor: 💡 添加遍历文件目录方法

--- server/router/file.js
+++ server/router/file.js
@@ -52,4 +52,74 @@ router.get('/isjs', async ctx => {
     }
 });
 
+/**
+ * 获取文件目录
+ */
+router.get('/display', async ctx => {
+
+});
+
+const ignoreFile = {
+    'node_modules': true,
+    '.git': true,
+    '.crui': false,
+};
+
+function displayFiles(filePaths) {
+    const filesArray = [];
+    const foldersArray = [
+        {
+            title: '/',
+            key: 1,
+            value: '',
+        }
+    ];
+    const stack = [''];
+    let key = 2;
+    function fileDisplayDeep(filePath, filesArray, foldersArray, isFirst) {
+        const files = fs.readdirSync(filePath);
+        if (!isFirst) {
+            stack.push(path.basename(filePath)); // 添加路由栈
+        }
+        files.forEach(filename => {
+            const urlStack = JSON.parse(JSON.stringify(stack)); // 深拷贝路由栈
+            urlStack.push(filename);
+            const url = path.join(...urlStack);
+            const filedir = path.join(filePath, filename);
+            const stats = fs.statSync(filedir);
+            if (stats.isFile() && !/^\..*$/.test(filename)) { // 文件
+                filesArray.push({
+                    title: filename,
+                    value: url,
+                    key: ++key,
+                });
+            }
+            if (stats.isDirectory() && !ignoreFile[filename]) { // 文件夹
+                const childrenFiles = [];
+                const childrenFolders = [];
+                key = key + 1;
+                filesArray.push({
+                    title: filename,
+                    key,
+                    value: url,
+                    children: childrenFiles,
+                });
+                foldersArray.push({
+                    title: filename,
+                    key,
+                    value: url,
+                    children: childrenFolders,
+                });
+                fileDisplayDeep(filedir, childrenFiles, childrenFolders);
+            }
+        });
+        stack.pop();
+    }
+    fileDisplayDeep(filePaths, filesArray, foldersArray, true);
+    return {
+        filesArray,
+        foldersArray,
+    };
+}
+
 module.exports = router;
