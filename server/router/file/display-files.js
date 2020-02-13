7fa97f2d257106908b8ed516f08d7e6bacbe659d
张志豪 <464485682@qq.com>
2020-02-13 17:09:50 +0800

    refactor: 💡 重构文件相关api结构

--- server/router/file/display-files.js
+++ server/router/file/display-files.js
@@ -1,68 +1,5 @@
-const Router = require('koa-router');
 const fs = require('fs');
 const path = require('path');
-
-const router = new Router();
-
-/**
- * 获取.crui/template下模板文件的目录
- */
-router.get('/template', async ctx => {
-    const base = path.join(process.cwd(), '.crui', 'template');
-    if (fs.existsSync(base)) {
-        const file = fs.readdirSync(base);
-        if (file.length === 0) {
-            ctx.error(0, '该目录下没有文件', null);
-        } else {
-            const data = [];
-            file.forEach((item, index) => {
-                if (/^.+\..+$/.test(item)) { // 判断是文件
-                    data.push({
-                        title: item,
-                        key: index + 1,
-                        value: item,
-                    });
-                }
-            });
-            ctx.success(200, '', data);
-        }
-    } else {
-        ctx.error(0, '找不到/.crui/template文件目录', null);
-    }
-});
-
-/**
- * 判断是否是js文件
- */
-router.get('/isjs', async ctx => {
-    const base = path.join(process.cwd(), ctx.query.url ? ctx.query.url : '');
-    if (fs.existsSync(base)) {
-        const stat = fs.statSync(base);
-        if (stat.isFile()) {
-            if (path.extname(base) === '.js') {
-                ctx.success(200, '验证成功', null);
-            } else {
-                ctx.error(-1, '不是js文件', null);
-            }
-        } else {
-            ctx.error(-1, '不是文件', null);
-        }
-    } else {
-        ctx.error(-1, '找不到该文件', null);
-    }
-});
-
-/**
- * 获取文件目录
- */
-router.get('/display', async ctx => {
-    const files = displayFiles(process.cwd());
-    try {
-        ctx.success(200, '获取成功', files);
-    } catch (err) {
-        ctx.error(0, err.message, null);
-    }
-});
 
 const ignoreFile = {
     'node_modules': true,
@@ -70,7 +7,7 @@ const ignoreFile = {
     '.crui': false,
 };
 
-function displayFiles(filePaths) {
+module.exports = function displayFiles(filePaths) {
     const filesArray = [];
     const foldersArray = [
         {
@@ -125,6 +62,4 @@ function displayFiles(filePaths) {
         filesArray,
         foldersArray,
     };
-}
-
-module.exports = router;
+};
