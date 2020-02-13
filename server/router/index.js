0ccf6cdae2e3d07df2d078745ae138d4d466b768
张志豪 <464485682@qq.com>
2020-02-13 17:12:32 +0800

    refactor: 💡 重构模板相关接口

--- server/router/index.js
+++ server/router/index.js
@@ -1,8 +1,10 @@
 const Router = require('koa-router');
 const file = require('./file');
+const template = require('./template');
 
 const router = new Router();
 
 router.use('/api/file', file.routes());
+router.use('/api/template', template.routes());
 
 module.exports = router;
