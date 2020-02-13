8d22587d0f9c9d6ce0d2c4c9ac9e9c127c8ee2e6
张志豪 <464485682@qq.com>
2020-02-13 17:17:34 +0800

    refactor: 💡 重构创建默认模板接口

--- server/router/template/index.js
+++ server/router/template/index.js
@@ -1,3 +1,27 @@
 const Router = require('koa-router');
+const path = require('path');
+const fs = require('fs-extra');
+const defaultTemplate = require('../../templateString/default-template');
 
 const router = new Router();
+
+/**
+ * 创建默认模板
+ */
+router.get('/default', async ctx => {
+    let { url, folderName, fileName, variable } = ctx.query;
+    let base = path.join(process.cwd(), url ? url : '');
+    variable = variable ? variable : 'Template';
+
+    // 创建文件夹
+    if (folderName) {
+        base = path.join(base, folderName);
+        if (fs.existsSync(base)) {
+            ctx.error(0, '该文件夹已存在', null);
+            return;
+        }
+    }
+    const script = defaultTemplate(variable);
+    fs.outputFileSync(path.join(base, fileName), script);
+    ctx.success(200, '创建成功', null);
+});
