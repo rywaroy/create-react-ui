ec210ce86b331374e4f7f34e0fe43fe5e1ad45b7
张志豪 <464485682@qq.com>
2020-02-15 19:05:15 +0800

    refactor: 💡 删除创建umi模板socket

--- server/socket/template/index.js
+++ server/socket/template/index.js
@@ -1,19 +1,7 @@
-const createUmiTemplate = require('./create-umi-template');
 const createCoustomTemplate = require('./create-custom-template');
 const { createSuccessMsg, createFailMsg, updateFiles } = require('../common');
 
 module.exports = function template(socket) {
-
-    // 创建umi模板
-    socket.on('create-umi-template', async data => {
-        try {
-            await createUmiTemplate(data);
-            createSuccessMsg(socket);
-            updateFiles(socket);
-        } catch (err) {
-            createFailMsg(socket, err);
-        }
-    });
 
     // 创建自定义模板
     socket.on('create-custom-template', async data => {
