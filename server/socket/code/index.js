97665e3c2bbfe3b1c6b664d9a43600bc514f0c2b
张志豪 <464485682@qq.com>
2020-02-19 16:09:18 +0800

    refactor: 💡 删除创建表格代码块socket

--- server/socket/code/index.js
+++ server/socket/code/index.js
@@ -1,17 +1,7 @@
-const createTableCode = require('./table-code');
 const createFormCode = require('./form-code');
 const { createSuccessMsg, createFailMsg } = require('../common');
 
 module.exports = function code(socket) {
-    // 创建table组件配置对象
-    socket.on('create-table-code', async data => {
-        try {
-            await createTableCode(data);
-            createSuccessMsg(socket);
-        } catch (err) {
-            createFailMsg(socket, err);
-        }
-    });
 
     // 创建form组件配置对象
     socket.on('create-form-code', async data => {
