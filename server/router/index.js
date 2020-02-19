8a71b0275207f9101f15ecdd48a934d286d4727a
张志豪 <464485682@qq.com>
2020-02-19 16:05:40 +0800

    feat: 🎸 添加代码块接口

--- server/router/index.js
+++ server/router/index.js
@@ -1,10 +1,12 @@
 const Router = require('koa-router');
 const file = require('./file');
 const template = require('./template');
+const code = require('./code');
 
 const router = new Router();
 
 router.use('/api/file', file.routes());
 router.use('/api/template', template.routes());
+router.use('/api/code', code.routes());
 
 module.exports = router;
