ee0708c9ddff6882859b14bae506448c6b68c266
张志豪 <464485682@qq.com>
2020-02-22 20:33:47 +0800

    refactor: 💡 重构socket

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,18 +1,8 @@
-const template = require('./template');
-const code = require('./code');
-
 module.exports = function createSocket(server) {
     const io = require('socket.io')(server);
 
-    io.on('connection', (socket) => {
-
+    io.on('connection', socket => {
         // 心跳
         socket.on('heart-link', () => {});
-
-        // 模板相关
-        template(socket);
-
-        // 代码块相关
-        code(socket);
     });
 };
