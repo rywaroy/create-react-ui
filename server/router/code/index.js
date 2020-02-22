b4a70fba2ef2428a89a7d07af70c7b13ce988241
张志豪 <464485682@qq.com>
2020-02-22 20:30:57 +0800

    refactor: 💡 重构创建表单代码块接口

--- server/router/code/index.js
+++ server/router/code/index.js
@@ -18,4 +18,18 @@ router.get('/table', async ctx => {
     }
 });
 
+/**
+ * 创建表单代码块
+ */
+router.get('/form', async ctx => {
+    const { url, code } = ctx.query;
+    const base = path.join(process.cwd(), url ? url : '');
+    if (fs.existsSync(base)) {
+        fs.appendFileSync(base, `\n\n${code}`, 'utf8');
+        ctx.success(200, '创建成功', null);
+    } else {
+        ctx.error(0, '找不到该文件', null);
+    }
+});
+
 module.exports = router;
