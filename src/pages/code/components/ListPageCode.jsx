9b9499ba465dd96f2fc272ce4c49ed49a4886f45
张志豪 <464485682@qq.com>
2020-03-03 19:09:36 +0800

    feat: 🎸 添加ListPage配置弹窗

--- src/pages/code/components/ListPageCode.jsx
+++ src/pages/code/components/ListPageCode.jsx
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
+import { Modal } from 'antd';
 import TemplateItem from '@/components/TemplateItem';
 
 class ListPageCode extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            configVisible: false,
+            configKey: Math.random(),
         };
     }
 
     render() {
+        const { configKey, configVisible } = this.state;
+
         return (
-            <TemplateItem
-                title="列表页面配置对象"
-                intro=""
-                imgClassName="listPageImg" />
+            <div>
+                <TemplateItem
+                    title="列表页面配置对象"
+                    imgClassName="listPageImg"
+                    add={this.openListPageCode} />
+                <Modal
+                    title="form组件配置"
+                    key={configKey}
+                    visible={configVisible}>
+                    <div />
+                </Modal>
+            </div>
+
         );
     }
 }
