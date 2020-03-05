c162cb7ade52ba092da5d73890560284ec59431c
张志豪 <464485682@qq.com>
2020-03-05 17:11:53 +0800

    feat: 🎸 页面标题配置

--- src/pages/code/components/ListPageModal.jsx
+++ src/pages/code/components/ListPageModal.jsx
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
-import { Modal } from 'antd';
+import { Modal, Input } from 'antd';
+import styles from '../index.less';
 
 class ListPageModal extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            title: '', // 页面标题
         };
     }
 
     render() {
         const { visible, onCancel } = this.props;
+        const { title } = this.state;
         return (
             <Modal
                 visible={visible}
                 title="配置"
                 width="1200px"
                 onCancel={() => { onCancel(); }}>
-                <div />
+                <div className={styles.listPage}>
+                    <div className={styles.title}>
+                        <Input placeholder="页面标题" className={styles.titleInput} value={title} onChange={e => this.setState({ title: e.target.value })} />
+                    </div>
+                </div>
             </Modal>
         );
     }
