0e6c591705fd798097d70335557a0309a1e88dd1
rywaroy <464485682@qq.com>
2020-03-08 18:30:27 +0800

    feat: 🎸 筛选表单组件展示

--- src/pages/code/components/ListPageCode/ListPageFilter.jsx
+++ src/pages/code/components/ListPageCode/ListPageFilter.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { Button } from 'antd';
+import GenerateForm from '@/components/GenerateForm';
+import styles from './index.less';
 
 class ListPageFilter extends Component {
     constructor(props) {
         super(props);
-        this.state = { };
+        this.state = {
+            formOption: [],
+        };
     }
 
     /**
@@ -14,12 +18,34 @@ class ListPageFilter extends Component {
 
     }
 
+    /**
+     * 模拟提交
+     */
+    handleSubmit = e => {
+        e.preventDefault();
+        this.generateForm.verify();
+    };
+
     render() {
+        const { formOption } = this.state;
         return (
             <div style={{ marginTop: '20px' }}>
                 <div>
                     筛选表单：
                     <Button type="primary" onClick={this.openAdd}>添加</Button>
+                </div>
+                <div className={styles.formBox}>
+                    <GenerateForm
+                        formSet={formOption}
+                        formType="filter"
+                        wrappedComponentRef={el => { this.generateForm = el; }}
+                        deleteItem={this.deleteItem}
+                    />
+                    {formOption.length > 0 && (
+                        <Button type="primary" onClick={this.handleSubmit} className={styles.testButton}>
+                            测试rules
+                        </Button>
+                    )}
                 </div>
 
             </div>
