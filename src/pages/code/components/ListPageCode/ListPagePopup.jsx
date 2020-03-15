0b80119bf41bd234838f0e71acd79ac1b8f47946
rywaroy <464485682@qq.com>
2020-03-15 21:05:00 +0800

    feat: 🎸 添加配置弹窗

--- src/pages/code/components/ListPageCode/ListPagePopup.jsx
+++ src/pages/code/components/ListPageCode/ListPagePopup.jsx
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
-import { Button } from 'antd';
+import { Button, Modal } from 'antd';
+import CreateForm from '@/components/CreateForm';
 
 class ListPagePopup extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            visible: false,
+            modalKey: Math.random(),
+        };
+    }
+
+    /**
+     * 打开弹窗配置弹窗
+     */
+    addPopup = () => {
+        this.setState({
+            visible: true,
+            modalKey: Math.random(),
+        });
     }
 
     render() {
+        const { visible, modalKey } = this.state;
+
         return (
             <div>
-                <Button type="primary">添加弹窗</Button>
+                <Button type="primary" onClick={this.addPopup}>添加弹窗</Button>
+                <Modal
+                    title="popup组件配置"
+                    width="1400px"
+                    visible={visible}
+                    key={modalKey}
+                    maskClosable={false}
+                    zIndex={1002}>
+                    <CreateForm
+                        type="modal" />
+                </Modal>
             </div>
         );
     }
