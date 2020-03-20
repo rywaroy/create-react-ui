0f8011159f298ba0abf231a34046e7807d513e33
rywaroy <464485682@qq.com>
2020-03-20 16:24:26 +0800

    feat: 🎸 修改code下的接口为post

--- src/services/code.js
+++ src/services/code.js
@@ -4,16 +4,12 @@ import axios from '@/utils/axios';
  * 创建表格代码块
  */
 export function createTableCode(params) {
-    return axios.get('code/table', {
-        params,
-    });
+    return axios.post('code/table', params);
 }
 
 /**
  * 创建表单代码块
  */
 export function createFormCode(params) {
-    return axios.get('code/form', {
-        params,
-    });
+    return axios.post('code/form', params);
 }
