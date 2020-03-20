d4d4a07083e528e8d87dbea515c447786e5f9fb3
rywaroy <464485682@qq.com>
2020-03-20 16:01:53 +0800

    refactor: 💡 重构template/umi接口,添加Controller

---
+++ server/controller/template/index.js
@@ -0,0 +1,7 @@
+const defaultController = require('./default');
+const umi = require('./umi');
+
+module.exports = {
+    default: defaultController,
+    umi,
+};
--- server/router/template/index.js
+++ server/router/template/index.js
@@ -5,8 +5,6 @@ const babelParser = require('@babel/parser');
 const traverse = require('@babel/traverse').default;
 const generate = require('@babel/generator').default;
 
-const umiModel = require('../../templateString/umi-model');
-const umiTemplate = require('../../templateString/umi-template');
 const createExportVisitor = require('./create-export-visitor');
 const templateController = require('../../controller/template');
 
@@ -20,28 +18,7 @@ router.get('/default', templateController.default);
 /**
  * 创建umi模板
  */
-router.get('/umi', async ctx => {
-    let { variable, namespace } = ctx.query;
-    const { url, folderName, fileName, oilConfig } = ctx.query;
-    let base = path.join(process.cwd(), url || '');
-    variable = variable || 'Template';
-    namespace = namespace || 'global';
-
-    // 创建文件夹
-    if (folderName) {
-        base = path.join(base, folderName);
-        if (fs.existsSync(base)) {
-            ctx.error(0, '该文件夹已存在', null);
-            return;
-        }
-    }
-    const script = umiTemplate(variable, namespace);
-    const modelscript = umiModel(namespace, oilConfig);
-
-    fs.outputFileSync(path.join(base, fileName), script);
-    fs.outputFileSync(path.join(base, 'model.js'), modelscript);
-    ctx.success(200, '创建成功', null);
-});
+router.get('/umi', templateController.umi);
 
 /**
  * 创建自定义模板
