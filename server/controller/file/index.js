5c66bd7538d011a0e85c9079ba6252a883d01a93
rywaroy <464485682@qq.com>
2020-03-20 15:31:52 +0800

    refactor: 💡 重构file/template接口,添加Controller

---
+++ server/controller/file/index.js
@@ -0,0 +1,5 @@
+const template = require('./template');
+
+module.exports = {
+    template,
+};
--- server/router/file/index.js
+++ server/router/file/index.js
@@ -2,35 +2,14 @@ const Router = require('koa-router');
 const fs = require('fs');
 const path = require('path');
 const displayFiles = require('./display-files');
+const fileController = require('../../controller/file');
 
 const router = new Router();
 
 /**
  * 获取.crui/template下模板文件的目录
  */
-router.get('/template', async ctx => {
-    const base = path.join(process.cwd(), '.crui', 'template');
-    if (fs.existsSync(base)) {
-        const file = fs.readdirSync(base);
-        if (file.length === 0) {
-            ctx.error(0, '该目录下没有文件', null);
-        } else {
-            const data = [];
-            file.forEach((item, index) => {
-                if (/^.+\..+$/.test(item)) { // 判断是文件
-                    data.push({
-                        title: item,
-                        key: index + 1,
-                        value: item,
-                    });
-                }
-            });
-            ctx.success(200, '', data);
-        }
-    } else {
-        ctx.error(0, '找不到/.crui/template文件目录', null);
-    }
-});
+router.get('/template', fileController.template);
 
 /**
  * 判断是否是js文件
