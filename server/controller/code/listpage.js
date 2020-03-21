0424ba3272a9d47ba5afbf8f8cb1fe1748c9bc45
rywaroy <464485682@qq.com>
2020-03-21 13:21:53 +0800

    feat: 🎸 生成基础map.js文件

--- server/controller/code/listpage.js
+++ server/controller/code/listpage.js
@@ -1,14 +1,21 @@
 const path = require('path');
 const fs = require('fs-extra');
 const oilListpageMap = require('../../templateString/oil-listpage-map');
+const oilListpageModel = require('../../templateString/oil-listpage-model');
 
 module.exports = async function listpage(ctx) {
-    const { url, name, pageOption } = ctx.request.body;
+    const { url, name, pageOption, namespace } = ctx.request.body;
     const { formCode, tableCode, popupForms } = pageOption;
-    const base = path.join(process.cwd(), url, name, 'map.js');
+    // map.js
+    const mapBase = path.join(process.cwd(), url, name, 'map.js');
     const mapString = oilListpageMap(formCode, tableCode, popupForms);
+
+    // model.js
+    const modelBase = path.join(process.cwd(), url, name, 'model.js');
+    const modelString = oilListpageModel(namespace);
     try {
-        fs.outputFileSync(base, mapString);
+        fs.outputFileSync(mapBase, mapString);
+        fs.outputFileSync(modelBase, modelString);
         ctx.success(200, '创建成功', null);
     } catch {
         ctx.success(0, '创建失败', null);
