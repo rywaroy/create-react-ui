2e00cf869f40b371df92aab329a50a18eb42438f
rywaroy <464485682@qq.com>
2020-03-21 13:00:33 +0800

    refactor: 💡 抽离模板代码

--- server/controller/code/listpage.js
+++ server/controller/code/listpage.js
@@ -1,13 +1,12 @@
 const path = require('path');
 const fs = require('fs-extra');
+const oilListpageMap = require('../../templateString/oil-listpage-map');
 
 module.exports = async function listpage(ctx) {
     const { url, name, pageOption } = ctx.request.body;
     const { formCode } = pageOption;
     const base = path.join(process.cwd(), url, name, 'map.js');
-    const mapString = `
-${formCode}
-`;
+    const mapString = oilListpageMap(formCode);
     try {
         fs.outputFileSync(base, mapString);
         ctx.success(200, '创建成功', null);
---
+++ server/templateString/oil-listpage-map.js
@@ -0,0 +1,5 @@
+module.exports = function oilListpageMap(formCode) {
+    return `
+${formCode}
+`;
+};
