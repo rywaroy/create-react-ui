ffa518caa6e9872277ec3101f69c4a430a108f50
rywaroy <464485682@qq.com>
2020-03-21 12:57:57 +0800

    feat: 🎸 创建map.js 添加filters代码

--- server/controller/code/listpage.js
+++ server/controller/code/listpage.js
@@ -1,4 +1,17 @@
+const path = require('path');
+const fs = require('fs-extra');
 
-module.exports = async function listpage() {
-    // const { url, code } = ctx.request.body;
+module.exports = async function listpage(ctx) {
+    const { url, name, pageOption } = ctx.request.body;
+    const { formCode } = pageOption;
+    const base = path.join(process.cwd(), url, name, 'map.js');
+    const mapString = `
+${formCode}
+`;
+    try {
+        fs.outputFileSync(base, mapString);
+        ctx.success(200, '创建成功', null);
+    } catch {
+        ctx.success(0, '创建失败', null);
+    }
 };
