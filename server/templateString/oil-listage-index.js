69075cd8cb9b15e9ddb31e4559e180a3ae5b486a
rywaroy <464485682@qq.com>
2020-03-22 19:05:24 +0800

    feat: 🎸 添加index.js 模板字符串

---
+++ server/templateString/oil-listage-index.js
@@ -0,0 +1,86 @@
+module.exports = function oilListpageIndex() {
+    return `
+import React from 'react';
+import { connect } from 'dva';
+import { Button, Table } from 'antd';
+import { listFilter, listColumn } from './map';
+import { ListFilter, SubHeader } from '@/components';
+import { Global_Pagination } from '@/lib/enum';
+
+class Dirver extends React.Component {
+
+    searchHandel = (searchFormData) => {
+        this.queryList({ pageNum: 1, searchFormData });
+    };
+
+    /**
+     * 查询列表
+     * @param {Object} params - 查询参数对象
+     */
+    queryList(params = {}) {
+        this.props.dispatch({
+            type: 'accountDriver/updateStateCall',
+            payload: { ...params },
+        }).then(() => {
+            this.props.dispatch({ type: 'accountDriver/queryList' });
+        });
+    }
+
+    /**
+     * 页码切换
+     * @param {Number} page - 页码
+     */
+    onPageChange = (page) => {
+        this.queryList({ pageNum: page });
+    }
+
+    /**
+     * 页码变化
+     * @param {Number} current - 当前页数
+     * @param {Number} size - 分页尺寸
+     */
+    onShowSizeChange = (current, size) => {
+        this.queryList({ pageNum: current, pageSize: size });
+    }
+
+    componentWillUnmount() {
+        this.props.dispatch({ type: 'accountDriver/resetState' });
+    }
+
+    componentDidMount() {
+        this.queryList();
+    }
+
+    render() {
+        const { accountDriver } = this.props;
+        const {
+            total,
+            pageNum,
+            pageSize,
+        } = accountDriver;
+        const pagination = {
+            ...Global_Pagination,
+            total,
+            current: pageNum,
+            pageSize,
+            onChange: this.onPageChange,
+            onShowSizeChange: this.onShowSizeChange
+        };
+
+        return (
+            <div className="bg-w">
+                <SubHeader title="司机账户">
+                    <Button type="primary" onClick={this.exportDriver}>导出</Button>
+                </SubHeader>
+                <div className="padding20">
+                    <ListFilter filters={listFilter(this)} onSearch={this.searchHandel} ref={el => this.listFilter = el}/>
+                    <Table className="mt10" columns={listColumn(this)} dataSource={accountDriver.listData} pagination={pagination} rowKey={r => r.partyId} />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default connect(({ accountDriver }) => ({ accountDriver }))(Dirver);    
+`;
+};
