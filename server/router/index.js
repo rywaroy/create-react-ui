a0fddc1d80b9ab632b78dd70a6fe5a977998b29b
rywaroy <464485682@qq.com>
2020-03-25 11:23:22 +0800

    fix: 🐛 修复react router 路由404bug,重定向到根路由

--- server/router/index.js
+++ server/router/index.js
@@ -5,6 +5,14 @@ const code = require('./code');
 
 const router = new Router();
 
+router.get('*', async (ctx, next) => {
+    if (ctx.response.status === 404) {
+        ctx.response.redirect('/');
+    } else {
+        next();
+    }
+});
+
 router.use('/api/file', file.routes());
 router.use('/api/template', template.routes());
 router.use('/api/code', code.routes());
