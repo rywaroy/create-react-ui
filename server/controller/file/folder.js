07d0f2a8f3d5aff50c651a131af2d412e239500e
rywaroy <464485682@qq.com>
2020-03-26 16:17:41 +0800

    feat: 🎸 文件夹目录接口添加文件夹判断

--- server/controller/file/folder.js
+++ server/controller/file/folder.js
@@ -1,5 +1,6 @@
 const glob = require('glob');
 const path = require('path');
+const fs = require('fs');
 
 module.exports = async function folder(ctx) {
     const base = ctx.query.base ? ctx.query.base : '/';
@@ -8,11 +9,13 @@ module.exports = async function folder(ctx) {
     });
     const data = list.map(item => {
         const url = path.join(base, item);
+        const stats = fs.statSync(url);
         return {
             id: url,
             value: url,
-            title: url,
+            title: item,
             pId: base,
+            isLeaf: stats.isFile(),
         };
     });
     ctx.success(200, '获取成功', data);
