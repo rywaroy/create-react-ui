6cef4de169d24fc1bdb45df1a16c847649ac2aab
rywaroy <464485682@qq.com>
2020-03-26 15:37:18 +0800

    feat: 🎸 请求数据,添加treeSelect展示组件

--- src/pages/publish/index.jsx
+++ src/pages/publish/index.jsx
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 import { Terminal } from 'xterm';
+import { TreeSelect } from 'antd';
+import { getFolder } from '@/services/file';
 
 class Publish extends Component {
     constructor(props) {
         super(props);
-        this.state = { };
+        this.state = {
+            value: undefined,
+            treeData: [],
+        };
+    }
+
+    /**
+     * 获取文件夹目录
+     */
+    getFolder(base = '/') {
+        getFolder({
+            base,
+        }).then(res => {
+            this.setState({
+                treeData: this.state.treeData.concat(res.data.data),
+            });
+        });
     }
 
     componentDidMount() {
@@ -13,12 +31,23 @@ class Publish extends Component {
         window.socket.on('term', msg => {
             this.term.write(msg);
         });
+        this.getFolder();
     }
 
     render() {
+        const { treeData, value } = this.state;
+
         return (
             <div>
-                <div id="terminal" />
+                <TreeSelect
+                    treeDataSimpleMode
+                    style={{ width: '100%' }}
+                    value={value}
+                    dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
+                    placeholder="Please select"
+                    treeData={treeData}
+                />
+                <div id="terminal" className="terminal" />
             </div>
         );
     }
