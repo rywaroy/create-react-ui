4792c983b62132203c86a670d703845b7335b2d3
rywaroy <464485682@qq.com>
2020-03-27 10:22:10 +0800

    feat: 🎸 更新svn目录命令

--- server/socket/publish/index.js
+++ server/socket/publish/index.js
@@ -1,11 +1,19 @@
 const execa = require('execa');
 
 module.exports = function publish(socket) {
-    socket.on('build', async () => {
-        socket.emit('term', '开始构建\n');
-        socket.emit('term', 'npm run build\n');
+    socket.on('build', async ({ svnBase }) => {
+        // npm run build 构建流程
+        socket.emit('term', '开始构建');
+        socket.emit('term', 'npm run build');
         socket.emit('build-start');
         await execa.command('npm run build');
         socket.emit('term', '构建完成\n');
+
+        // 更新svn目录
+        socket.emit('term', '更新svn目录');
+        socket.emit('term', 'svn update');
+        await execa.command('svn update', {
+            cwd: svnBase,
+        });
     });
 };
