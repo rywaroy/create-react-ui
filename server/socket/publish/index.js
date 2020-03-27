dc5ead08f600f51c89f6e241e8e573ab24eec8be
rywaroy <464485682@qq.com>
2020-03-27 10:10:14 +0800

    feat: 🎸 添加npm run build 构建

--- server/socket/publish/index.js
+++ server/socket/publish/index.js
@@ -1,8 +1,11 @@
 const execa = require('execa');
 
 module.exports = function publish(socket) {
-    socket.on('build', () => {
-        const { stdout } = execa.commandSync('echo 1');
-        socket.emit('term', stdout);
+    socket.on('build', async () => {
+        socket.emit('term', '开始构建\n');
+        socket.emit('term', 'npm run build\n');
+        socket.emit('build-start');
+        await execa.command('npm run build');
+        socket.emit('term', '构建完成\n');
     });
 };
