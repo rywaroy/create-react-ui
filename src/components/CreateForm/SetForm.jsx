ddbe7e4a10b6d140178b863f63d54dc703fef226
rywaroy <464485682@qq.com>
2020-04-05 15:06:44 +0800

    refactor: 💡 提取setForm组件公用的option

--- src/components/CreateForm/SetForm.jsx
+++ src/components/CreateForm/SetForm.jsx
@@ -1,51 +1,8 @@
 import React, { Component } from 'react';
 import { Modal, Select, Input, Form, Radio, InputNumber, Button } from 'antd';
+import { TYPES, initialValueOptions, colonOptions, addonAfterOptions, formItemLayoutOptions, ruleTypes, mockData } from '@/utils/enum';
 
 const { Option } = Select;
-
-const TYPES = [
-    { value: 'label', label: '文字 label' },
-    { value: 'input', label: '输入框 input' },
-    { value: 'select', label: '选择框 select' },
-    { value: 'inputnumber', label: '数字输入框 inputnumber' },
-    { value: 'password', label: '密码框 password' },
-    { value: 'datepicker', label: '日期选择框 datepicker' },
-    { value: 'monthpicker', label: '月份选择框 monthpicker' },
-    { value: 'rangepicker', label: '时间区间选择框 rangepicker' },
-    // { value: 'checkbox', label: '多选 checkbox' },
-    { value: 'checkboxgroup', label: '输入框组 checkboxgroup' },
-    { value: 'textarea', label: '文本框 textarea' },
-    { value: 'radiogroup', label: '单选组 radiogroup' },
-];
-
-const initialValueOptions = [
-    { label: 'false', value: false },
-    { label: 'true', value: true },
-];
-
-const colonOptions = [
-    { label: 'true', value: true },
-    { label: 'false', value: false },
-];
-
-const addonAfterOptions = [
-    { label: 'false', value: false },
-    { label: 'true', value: true },
-];
-
-const formItemLayoutOptions = [
-    { label: 'false', value: false },
-    { label: '变量', value: '变量' },
-    { label: '数值', value: '数值' },
-];
-
-const ruleTypes = ['required', 'max', 'min', 'len'];
-
-const mockData = [
-    { value: '测试数据1', label: '测试数据1' },
-    { value: '测试数据2', label: '测试数据2' },
-    { value: '测试数据3', label: '测试数据3' },
-];
 
 class SetForm extends Component {
     constructor(props) {
