557463e1be1719b9f28e1fae2b286f3360ef3fca
rywaroy <464485682@qq.com>
2020-04-12 16:36:49 +0800

    feat: 🎸 判断入口,是文件夹则遍历获取所有js文件

--- server/socket/document/index.js
+++ server/socket/document/index.js
@@ -1,7 +1,15 @@
-
+const path = require('path');
+const fs = require('fs');
+const getTargetFile = require('./getTargetFile');
 
 module.exports = function document(socket) {
-    socket.on('create-document', ({ entry, output }) => {
-        console.log(entry, output);
+    socket.on('create-document', ({ entry }) => {
+        const entryBase = path.join(process.cwd(), entry);
+        const stat = fs.statSync(entryBase);
+        let files = [];
+        if (stat.isDirectory()) {
+            files = getTargetFile(entryBase);
+        }
+        return files;
     });
 };
