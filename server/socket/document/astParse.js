7bbe56ec74eb447c63d194dd59beec2a78638ff0
rywaroy <464485682@qq.com>
2020-04-13 09:16:40 +0800

    feat: 🎸 遍历ast 解析函数式组件

--- server/socket/document/astParse.js
+++ server/socket/document/astParse.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const babelParser = require('@babel/parser');
+const traverse = require('@babel/traverse').default;
 
 module.exports = function astParse(base) {
     const ast = babelParser.parse(fs.readFileSync(base, 'utf-8'), {
@@ -9,5 +10,18 @@ module.exports = function astParse(base) {
             'jsx',
         ],
     });
-    console.log(ast);
+    traverse(ast, {
+        ExportDefaultDeclaration(path) {
+            if (path.node.declaration.type === 'FunctionDeclaration') {
+                /**
+                * 导出函数式组件
+                * @example
+                * export default function Component(props) {
+                *
+                * }
+                */
+                console.log(path.node.declaration);
+            }
+        },
+    });
 };
