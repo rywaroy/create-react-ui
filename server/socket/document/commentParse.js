fb7e101f58c0425a5aebf312443380ae98963654
rywaroy <464485682@qq.com>
2020-04-13 09:24:41 +0800

    feat: 🎸 添加行级注释解析

--- server/socket/document/commentParse.js
+++ server/socket/document/commentParse.js
@@ -1,3 +1,51 @@
+const rules = {
+    '@version': {
+        name: 'version',
+        cn: '版本号',
+        simple: true,
+    },
+    '@author': {
+        name: 'author',
+        cn: '作者',
+        simple: true,
+    },
+    '@param': {
+        name: 'param',
+        cn: '参数',
+        simple: false,
+    },
+    '@returns': {
+        name: 'returns',
+        cn: '返回',
+        simple: false,
+    },
+    '@intro': {
+        name: 'intro',
+        cn: '介绍',
+        simple: true,
+    },
+    '@url': {
+        name: 'url',
+        cn: '地址',
+        simple: true,
+    },
+    '@image': {
+        name: 'image',
+        cn: '图片',
+        simple: true,
+    },
+    '@txt': {
+        name: 'txt',
+        cn: '文本',
+        simple: true,
+    },
+    '@title': {
+        name: 'title',
+        cn: '标题',
+        simple: true,
+    },
+};
+
 /**
  * 过滤注释，返回注释解析对象
  * @param {Array} comments - 注释数组
@@ -44,5 +92,42 @@ function filterCommentBlock(comment) {
  * @returns {Object | String}
  */
 function filterCommentLine(line) {
-    return line;
+    // 验证是否包含文档规则@，否则直接返回字符串
+    if (/@[a-z]+/.test(line)) {
+        let flag = false;
+        let obj;
+        Object.keys(rules).forEach(key => {
+            // 遍历规则，查找
+            if (line.indexOf(key) > -1) {
+                flag = true;
+                if (rules[key].simple) {
+                    obj = {
+                        name: key,
+                        value: line.replace(new RegExp(`\\s*${key}\\s+`), ''),
+                        ...rules[key],
+                    };
+                } else {
+                    // 解析复杂规则如 @param @returns
+                    obj = filterRules(key, line);
+                }
+            }
+        });
+        if (!flag) { // 未匹配规则，则返回字符串
+            return {
+                name: 'txt',
+                value: line,
+                cn: '文本',
+            };
+        }
+        return obj;
+    }
+    return {
+        name: 'txt',
+        value: line,
+        cn: '文本',
+    };
+}
+
+function filterRules() {
+
 }
