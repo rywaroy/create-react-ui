c4a90c0011c722eee2a0ad8adce560dcffc2429e
rywaroy <464485682@qq.com>
2020-04-13 09:20:24 +0800

    style: 💄 添加注释

--- server/socket/document/commentParse.js
+++ server/socket/document/commentParse.js
@@ -1,3 +1,8 @@
+/**
+ * 过滤注释，返回注释解析对象
+ * @param {Array} comments - 注释数组
+ * @returns {Object}
+ */
 module.exports = function commentParse(comments) {
     const commentArray = [];
     comments.forEach(comment => {
@@ -14,10 +19,20 @@ module.exports = function commentParse(comments) {
     return commentArray;
 };
 
+/**
+ * 过滤块级注释，返回单行内容
+ * @param {String} str - 注释字符串
+ * @returns {Array}
+ */
 function filterCommentBlock(comment) {
     return comment;
 }
 
+/**
+ * 过滤行级注释
+ * @param {String} line - 单行注释字符串
+ * @returns {Object | String}
+ */
 function filterCommentLine(line) {
     return line;
 }
