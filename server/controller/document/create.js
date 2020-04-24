d9c4ecda3f31aa2f5ba692430bfc305bd3145190
rywaroy <464485682@qq.com>
2020-04-24 09:21:42 +0800

    feat: 🎸 文件上传添加非组件文件判断

--- server/controller/document/create.js
+++ server/controller/document/create.js
@@ -6,6 +6,17 @@ module.exports = async function create(ctx) {
     // const { output } = ctx.request.body;
     const code = fs.readFileSync(file.path, 'utf-8');
     const fileObj = astParse(null, code);
-    console.log(fileObj);
-    ctx.success(200, '成功', null);
+    const { isFunction, isClass, props, main } = fileObj;
+    if (isFunction && !props) {
+        // 如果是函数，如果没有props，代表不是函数组件，不生成md
+        ctx.success(0, '该文件不是组件', null);
+    } else if (isClass && !main && !props) {
+        // 如果是类，如果没有props且也没有注释，不生成md
+        ctx.success(0, '暂无解析数据', null);
+    } else if (!main && !props) {
+        // 不是函数也不是类，可能是工具库文件
+        ctx.success(0, '该文件不是组件', null);
+    } else {
+        ctx.success(200, '成功', null);
+    }
 };
