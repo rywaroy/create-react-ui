1fa0b453a9a5f06df374f043b39e9ba3c0c276a3
rywaroy <464485682@qq.com>
2020-04-30 14:48:56 +0800

    refactor: 💡 重构为单独缓存文件

--- server/controller/configlist/label.js
+++ server/controller/configlist/label.js
@@ -2,23 +2,16 @@ const path = require('path');
 const fs = require('fs-extra');
 
 module.exports = async function label(ctx) {
-    const configPath = path.join(process.cwd(), 'node_modules/.cache/crui/config.json');
-    const configExists = fs.existsSync(configPath);
-    if (configExists) {
-        const config = fs.readJsonSync(configPath);
-        const { label } = config;
-        if (label) {
-            ctx.success(200, '获取成功', label);
-        } else {
-            config.label = { display: false, list: [] };
-            fs.writeJsonSync(configPath, config);
-            ctx.success(200, '获取成功', { display: false, list: [] });
-        }
+    const labelPath = path.join(process.cwd(), 'node_modules/.cache/crui/label.json');
+    const labelExists = fs.existsSync(labelPath);
+    if (labelExists) {
+        const label = fs.readJsonSync(labelPath);
+        ctx.success(200, '获取成功', label);
     } else {
-        fs.writeJsonSync(configPath, { label: {
+        fs.writeJsonSync(labelPath, {
             display: false,
             list: [],
-        } });
+        });
         ctx.success(200, '获取成功', { display: false, list: [] });
     }
 };
