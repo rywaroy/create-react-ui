e6f5de0c20261b76b0c945bf93d241279097930c
rywaroy <464485682@qq.com>
2020-05-06 11:03:36 +0800

    feat: 🎸 添加修改label名功能

--- src/pages/configlist/index.jsx
+++ src/pages/configlist/index.jsx
@@ -1,36 +1,114 @@
 import React, { Component } from 'react';
-import { Row, Col, Table, Card } from 'antd';
+import { Row, Col, Table, Card, Modal, Input, message } from 'antd';
 import { connect } from 'dva';
-
-const columns = [
-    {
-        title: 'label',
-        dataIndex: 'name',
-    },
-    {
-        title: '操作',
-        key: 'option',
-    },
-];
+import { patchLabelConfig } from '@/services/configlist';
+import styles from './index.less';
 
 class ConfigList extends Component {
-    componentDidMount() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            labelName: '',
+            labelId: '',
+            labelVisible: false,
+        };
+    }
+
+    columns = [
+        {
+            title: 'label',
+            dataIndex: 'name',
+        },
+        {
+            title: '操作',
+            key: 'id',
+            render: record => (
+                    <>
+                        <span className={styles.optBtn} onClick={() => this.openLabel(record)}>修改</span>
+                        <span className={styles.optBtn}>删除</span>
+                    </>
+            ),
+        },
+    ];
+
+    /**
+     * 获取label配置列表
+     */
+    getLabelConfig() {
         this.props.dispatch({
             type: 'global/getLabelConfig',
         });
     }
 
+    /**
+     * 打开label编辑弹窗
+     */
+    openLabel({ id, name }) {
+        this.setState({
+            labelId: id,
+            labelName: name,
+            labelVisible: true,
+        });
+    }
+
+    /**
+     * 打开label编辑弹窗
+     */
+    closeLabel = () => {
+        this.setState({
+            labelVisible: false,
+        });
+    }
+
+    /**
+     * 编辑label
+     */
+    onChangeLabel = e => {
+        this.setState({
+            labelName: e.target.value,
+        });
+    }
+
+    /**
+     * 确认修改babel
+     */
+    confirmChangeLabel = () => {
+        const { labelId, labelName } = this.state;
+        patchLabelConfig({
+            id: labelId,
+            name: labelName,
+        }).then(() => {
+            message.success('修改成功');
+            this.closeLabel();
+            this.getLabelConfig();
+        });
+    }
+
+    componentDidMount() {
+        this.getLabelConfig();
+    }
+
     render() {
         const { labelList } = this.props.global;
+        const { labelName, labelVisible } = this.state;
+
         return (
             <div>
                 <Row>
                     <Col span={10}>
                         <Card title="label配置" bordered={false}>
-                            <Table columns={columns} dataSource={labelList} rowKey="id" />
+                            <Table columns={this.columns} dataSource={labelList} rowKey="id" />
                         </Card>
                     </Col>
                 </Row>
+                <Modal
+                    visible={labelVisible}
+                    onCancel={this.closeLabel}
+                    onOk={this.confirmChangeLabel}>
+                    <div style={{ paddingTop: '20px' }}>
+                        <Input placeholder="请输入label" maxLength={10} value={labelName} onChange={this.onChangeLabel} />
+                    </div>
+                </Modal>
             </div>
         );
     }
--- src/services/configlist.js
+++ src/services/configlist.js
@@ -8,3 +8,10 @@ export function getLabelConfig(params) {
         params,
     });
 }
+
+/**
+ * 修改label配置
+ */
+export function patchLabelConfig(params) {
+    return axios.patch('/configlist/label', params);
+}
