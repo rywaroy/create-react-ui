89d7fa43156ad039acffa223f13a5c290f7ad52b
rywaroy <464485682@qq.com>
2020-05-06 10:20:42 +0800

    feat: 🎸 label配置表格

--- src/pages/configlist/index.jsx
+++ src/pages/configlist/index.jsx
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
+import { Row, Col, Table, Card } from 'antd';
+
+const columns = [
+    {
+        title: 'label',
+        dataIndex: 'name',
+    },
+    {
+        title: '操作',
+        key: 'option',
+    },
+];
 
 class ConfigList extends Component {
     constructor(props) {
         super(props);
-        this.state = { };
+        this.state = {
+            data: [
+                {
+                    name: '测试1',
+                    id: 1,
+                },
+                {
+                    name: '测试2',
+                    id: 2,
+                },
+                {
+                    name: '测试3',
+                    id: 3,
+                },
+            ],
+        };
     }
 
     render() {
+        const { data } = this.state;
         return (
-            <div>configlist</div>
+            <div>
+                <Row>
+                    <Col span={10}>
+                        <Card title="label配置" bordered={false}>
+                            <Table columns={columns} dataSource={data} />
+                        </Card>
+                    </Col>
+                </Row>
+            </div>
         );
     }
 }
