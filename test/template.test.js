171f5a70e4ab0741aac256364f5b87549020709a
rywaroy <464485682@qq.com>
2020-05-12 09:43:11 +0800

    test: 💍 添加 /template/umi 接口测试

--- test/template.test.js
+++ test/template.test.js
@@ -19,4 +19,10 @@ describe('测试template接口', () => {
         expect(res.status).toBe(200);
         expect(fs.statSync('test/case/template_default.js').isFile()).toBeTruthy();
     });
+
+    it('/template/umi 接口测试', async () => {
+        const res = await server.get('/api/template/umi?fileName=template_umi.js&url=test/case');
+        expect(res.status).toBe(200);
+        expect(fs.statSync('test/case/template_umi.js').isFile()).toBeTruthy();
+    });
 });
