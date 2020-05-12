ba9db6c9f3a41e0993beb46fd5850c2e7d5911d3
rywaroy <464485682@qq.com>
2020-05-12 09:51:24 +0800

    test: 💍 添加 /template/custom 接口测试

--- test/template.test.js
+++ test/template.test.js
@@ -7,10 +7,25 @@ let server;
 beforeAll(() => {
     server = request(app.listen());
     fs.emptyDirSync('test/case');
+    fs.outputFileSync('.crui/template/custom.js', `
+import React, { Component } from 'react';
+
+class Custom extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Custom;`);
 });
 
 afterAll(() => {
     fs.removeSync('test/case');
+    fs.removeSync('.crui/template');
 });
 
 describe('测试template接口', () => {
@@ -24,5 +39,11 @@ describe('测试template接口', () => {
         const res = await server.get('/api/template/umi?fileName=template_umi.js&url=test/case');
         expect(res.status).toBe(200);
         expect(fs.statSync('test/case/template_umi.js').isFile()).toBeTruthy();
+    });
+
+    it('/template/custom 接口测试', async () => {
+        const res = await server.get('/api/template/custom?url=test/case');
+        expect(res.status).toBe(200);
+        expect(fs.statSync('test/case/custom.js').isFile()).toBeTruthy();
     });
 });
