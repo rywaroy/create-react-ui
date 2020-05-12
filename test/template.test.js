bfd0b67167d8b26e3252dfb28949b57f5771c6cb
rywaroy <464485682@qq.com>
2020-05-12 09:41:26 +0800

    test: 💍 添加 /template/default 接口测试

---
+++ test/template.test.js
@@ -0,0 +1,22 @@
+const request = require('supertest');
+const fs = require('fs-extra');
+const app = require('../server/app');
+
+let server;
+
+beforeAll(() => {
+    server = request(app.listen());
+    fs.emptyDirSync('test/case');
+});
+
+afterAll(() => {
+    fs.removeSync('test/case');
+});
+
+describe('测试template接口', () => {
+    it('/template/default 接口测试', async () => {
+        const res = await server.get('/api/template/default?fileName=template_default.js&url=test/case');
+        expect(res.status).toBe(200);
+        expect(fs.statSync('test/case/template_default.js').isFile()).toBeTruthy();
+    });
+});
