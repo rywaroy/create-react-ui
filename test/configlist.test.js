7fb83285af3ba26129192cac7cc3592c67d34b04
rywaroy <464485682@qq.com>
2020-05-18 09:27:24 +0800

    test: 💍 添加 /configlist/label POST 接口测试

--- test/configlist.test.js
+++ test/configlist.test.js
@@ -6,16 +6,25 @@ let server;
 
 beforeAll(() => {
     server = request(app.listen());
-    fs.ensureFileSync('node_modules/.cache/curi/label.json');
+    fs.ensureDirSync('node_modules/.cache/crui');
 });
 
 afterAll(() => {
-    fs.removeSync('node_modules/.cache/curi/label.json');
+    fs.removeSync('node_modules/.cache/crui');
 });
 
 describe('测试configlist接口', () => {
-    it('/configlist/label get 接口测试', async () => {
+    it('/configlist/label GET 接口测试', async () => {
         const res = await server.get('/api/configlist/label');
         expect(res.status).toBe(200);
+    });
+
+    it('/configlist/label POST 接口测试', async () => {
+        const res = await server
+            .post('/api/configlist/label')
+            .send({ name: 'test' });
+        expect(res.status).toBe(200);
+        const res2 = await server.get('/api/configlist/label');
+        expect(res2.body.data.list.length).toBe(1);
     });
 });
