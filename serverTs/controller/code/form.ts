cae5288be9a3db3f4500acb2eb57c9f7b8483676
rywaroy <464485682@qq.com>
2020-06-17 10:45:12 +0800

    refactor: ts重构code from接口

---
+++ serverTs/controller/code/form.ts
@@ -0,0 +1,20 @@
+import path from 'path';
+import fs from 'fs';
+import IContext from '../../types/context';
+import codeFormat from '../../utils/codeFormat';
+
+interface IBody {
+    url: string;
+    code: string;
+}
+
+export default async function form(ctx: IContext) {
+    const { url, code }: IBody = ctx.request.body;
+    const base = path.join(process.cwd(), url || '');
+    if (fs.existsSync(base)) {
+        fs.appendFileSync(base, `\n\n${codeFormat(code)}`, 'utf8');
+        ctx.success(200, '创建成功', null);
+    } else {
+        ctx.error(0, '找不到该文件', null);
+    }
+};
