7621bf70ea6505332b116516e65c78d6f9f9886a
rywaroy <464485682@qq.com>
2020-06-19 15:44:39 +0800

    refactor: ts 重构

---
+++ serverTs/controller/configlist/displayLabel.ts
@@ -0,0 +1,22 @@
+import path from 'path';
+import fs from 'fs-extra';
+import IContext from '../../types/context';
+import { ILabelJson } from '../../types/label';
+
+interface IBody {
+    display: boolean;
+}
+
+export default async function label(ctx: IContext) {
+    const { display }: IBody = ctx.request.body;
+    const labelPath = path.join(process.cwd(), 'node_modules/.cache/crui/label.json');
+    const labelExists = fs.existsSync(labelPath);
+    if (labelExists) {
+        const label: ILabelJson = fs.readJsonSync(labelPath);
+        label.display = display;
+        fs.writeJsonSync(labelPath, label);
+        ctx.success(200, '修改成功');
+    } else {
+        ctx.error(0, '修改失败');
+    }
+}
