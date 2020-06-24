a727992ab281e397890b1673b3bd436331669fc6
rywaroy <464485682@qq.com>
2020-06-24 18:15:29 +0800

    refactor: ts重构

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 import io from 'socket.io-client';
 import 'antd/dist/antd.css';
 
+declare global {
+    interface Window {
+        socket: any;
+    }
+}
+
 window.socket = io('http://localhost:2019');
 
 export const dva = {
