459cf19d140e05d21ed9bf6ac61d57be01476f55
rywaroy <464485682@qq.com>
2020-06-27 20:37:16 +0800

    feat: socket

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,3 @@
-import io from 'socket.io-client';
-import 'antd/dist/antd.css';
-
-declare global {
-    interface Window {
-        socket: any;
-    }
-}
-
-window.socket = io('http://localhost:2019');
-
 export const dva = {
     config: {
         onError(err) {
