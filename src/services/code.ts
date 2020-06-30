c577b142a732bb3597e1e366b7803dcf3f1941b4
rywaroy <464485682@qq.com>
2020-06-30 21:12:01 +0800

    refactor: ts重构

--- src/services/code.ts
+++ src/services/code.ts
@@ -1,15 +1,16 @@
 import axios from '@/utils/axios';
+import { IFormCode, ITableCode } from '@/types/code';
 
 /**
  * 创建表格代码块
  */
-export function createTableCode(params) {
+export function createTableCode(params: ITableCode) {
     return axios.post('code/table', params);
 }
 
 /**
  * 创建表单代码块
  */
-export function createFormCode(params) {
+export function createFormCode(params: IFormCode) {
     return axios.post('code/form', params);
 }
