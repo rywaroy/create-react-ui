ce5d94da61f58ca06e48f16631c3326c40314899
rywaroy <464485682@qq.com>
2020-06-30 21:51:35 +0800

    feat: interface

--- src/types/code.ts
+++ src/types/code.ts
@@ -7,3 +7,37 @@ export interface ITableCode {
   url: string;
   code: string;
 }
+
+export interface IFormItem {
+  type: string;
+  label: string;
+  name: string;
+  initialValue?: string;
+  span?: number;
+  formItemLayout?: IFormItemLayout;
+  formItemLayoutText?: string;
+  rules?: IFormRule[];
+  selectOptions?: IMockData[];
+  checkboxOptions?: IMockData[];
+  radioOptions?: IMockData[];
+}
+
+export interface IFormItemLayout {
+  labelCol: {
+    span: number;
+  };
+  wrapperCol: {
+    span: number;
+  }
+}
+
+export interface IFormRule {
+  rule?: string;
+  content?: string | boolean;
+  message?: string;
+}
+
+export interface IMockData {
+  value: string;
+  label: string;
+}
