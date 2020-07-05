a857946242db2224d2bf3ea040e102f68aae2cd3
rywaroy <464485682@qq.com>
2020-07-05 14:38:00 +0800

    refactor: 重构创建form代码逻辑，将生成逻辑放到后端

--- server/controller/code/form.ts
+++ server/controller/code/form.ts
@@ -1,18 +1,16 @@
 import path from 'path';
 import fs from 'fs';
+import { IFormCodeParams } from '../../types/code';
 import IContext from '../../types/context';
 import codeFormat from '../../utils/codeFormat';
-
-interface IBody {
-    url: string;
-    code: string;
-}
+import formCodeTemplate from '../../templateString/form-code-template';
 
 export default async function form(ctx: IContext) {
     // @ts-ignore for travis
-    const { url, code }: IBody = ctx.request.body;
+    const { url, options, name, labelCol, wrapperCol }: IFormCodeParams = ctx.request.body;
     const base = path.join(process.cwd(), url || '');
     if (fs.existsSync(base)) {
+        const code = formCodeTemplate(options, name, labelCol, wrapperCol);
         fs.appendFileSync(base, `\n\n${codeFormat(code)}`, 'utf8');
         ctx.success(200, '创建成功', null);
     } else {
