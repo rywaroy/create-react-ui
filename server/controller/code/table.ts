a977a91bf6dad3f5053991b0a51857d2ee7f0ccb
rywaroy <464485682@qq.com>
2020-07-05 13:17:15 +0800

    refactor: 重构生成表格代码逻辑，将生成逻辑放到后端

--- server/controller/code/table.ts
+++ server/controller/code/table.ts
@@ -1,18 +1,16 @@
 import path from 'path';
 import fs from 'fs';
+import { ITableCodeParams } from '../../types/code';
 import IContext from '../../types/context';
 import codeFormat from '../../utils/codeFormat';
-
-interface IBody {
-    url: string;
-    code: string;
-}
+import tableCodeTemplate from '../../templateString/table-code-template';
 
 export default async function table(ctx: IContext) {
     // @ts-ignore for travis
-    const { url, code }: IBody = ctx.request.body;
+    const { url, columns, variable }: ITableCodeParams = ctx.request.body;
     const base = path.join(process.cwd(), url || '');
     if (fs.existsSync(base)) {
+        const code = tableCodeTemplate(columns, variable);
         fs.appendFileSync(base, `\n\n${codeFormat(code)}`, 'utf8');
         ctx.success(200, '创建成功', null);
     } else {
