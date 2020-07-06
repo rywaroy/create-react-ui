121cb632cf7e87f048b22ecaaf086148b5ca5b9c
rywaroy <464485682@qq.com>
2020-07-06 19:38:48 +0800

    refactor: ts重构

--- server/templateString/oil-listpage-map.ts
+++ server/templateString/oil-listpage-map.ts
@@ -1,4 +1,22 @@
-export default function oilListpageMap(formCode: string, tableCode: string, popupFormsCode: string) {
+import { IFormObject, ITableObject } from '../types/code';
+import formCodeTemplate from './form-code-template';
+import tableCodeTemplate from './table-code-template';
+
+export default function oilListpageMap(formObject: IFormObject, tableObject: ITableObject, popupForms: IFormObject[]) {
+    // 表单代码
+    const { options, name, labelCol, wrapperCol } = formObject;
+    const formCode = formCodeTemplate(options, name, labelCol, wrapperCol);
+
+    // 表格代码
+    const { columns, variable } = tableObject;
+    const tableCode = tableCodeTemplate(columns, variable);
+
+    // 弹窗代码
+    const popupFormsCode = popupForms.map(item => {
+        const { options, name, labelCol, wrapperCol } = item;
+        return formCodeTemplate(options, name, labelCol, wrapperCol);
+    }).join('\n\n');
+
     return `
 ${formCode}
 
