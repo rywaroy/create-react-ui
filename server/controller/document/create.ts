5955be234bff405ca5b7e60b22a2de2bf1698605
rywaroy <464485682@qq.com>
2020-07-09 21:23:05 +0800

    fix: 修复babel parse失败报错

--- server/controller/document/create.ts
+++ server/controller/document/create.ts
@@ -11,22 +11,26 @@ export default async function create(ctx: IContext) {
     const { output } = ctx.request.body;
     const code = fs.readFileSync(file.path, 'utf-8');
     const fileObj = astParse(null, code);
-    const { isFunction, isClass, props, main } = fileObj;
-    if (isFunction && !props) {
-        // 如果是函数，如果没有props，代表不是函数组件，不生成md
-        ctx.status = 400;
-        ctx.error(0, '该文件不是组件', null);
-    } else if (isClass && !main && !props) {
-        // 如果是类，如果没有props且也没有注释，不生成md
-        ctx.status = 400;
-        ctx.error(0, '暂无解析数据', null);
-    } else if (!main && !props) {
-        // 不是函数也不是类，可能是工具库文件
-        ctx.status = 400;
-        ctx.error(0, '该文件不是组件', null);
+    if (typeof fileObj === 'boolean') {
+        ctx.error(0, '该文件解析报错', null);
     } else {
-        const name = getComponentName(fileObj);
-        createMd(fileObj, name, output);
-        ctx.success(200, '成功', null);
+        const { isFunction, isClass, props, main } = fileObj;
+        if (isFunction && !props) {
+        // 如果是函数，如果没有props，代表不是函数组件，不生成md
+            ctx.status = 400;
+            ctx.error(0, '该文件不是组件', null);
+        } else if (isClass && !main && !props) {
+        // 如果是类，如果没有props且也没有注释，不生成md
+            ctx.status = 400;
+            ctx.error(0, '暂无解析数据', null);
+        } else if (!main && !props) {
+        // 不是函数也不是类，可能是工具库文件
+            ctx.status = 400;
+            ctx.error(0, '该文件不是组件', null);
+        } else {
+            const name = getComponentName(fileObj);
+            createMd(fileObj, name, output);
+            ctx.success(200, '成功', null);
+        }
     }
 }
