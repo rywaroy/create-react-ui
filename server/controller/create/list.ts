fe41a3f860289faf0a2cce3d17dd1aadd4bf106f
rywaroy <464485682@qq.com>
2020-08-08 16:17:01 +0800

    feat: 添加判断是否是空文件字段

--- server/controller/create/list.ts
+++ server/controller/create/list.ts
@@ -5,10 +5,15 @@ import IContext from '../../types/context';
 
 export default async function list(ctx: IContext) {
     let generator: any;
+    let isEmpty = true;
     if (fs.existsSync(path.join(process.cwd(), 'package.json'))) {
         generator = add();
+        isEmpty = false;
     } else {
         generator = create();
     }
-    ctx.success(200, '获取成功', generator.getModulePrompts());
+    ctx.success(200, '获取成功', {
+        list: generator.getModulePrompts(),
+        isEmpty,
+    });
 }
