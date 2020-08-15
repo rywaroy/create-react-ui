0946a8f5c952f8d42e930292cedac3b16343a3ad
rywaroy <464485682@qq.com>
2020-08-16 00:11:25 +0800

    feat: 根组件

--- src/components/materials/index.tsx
+++ src/components/materials/index.tsx
@@ -1,5 +1,24 @@
 import { Row, Col } from 'antd';
 import { IMaterial } from '@/types/making';
+import BaseContent from './BaseContent';
+
+export const baseContent: IMaterial = {
+    name: '根组件',
+    tag: 'BaseContent',
+    from: 'xxx',
+    id: 1,
+    component: BaseContent,
+    intro: '根目录',
+    props: {},
+    defaultProps: {
+        style: {
+            width: '100%',
+            height: '100%',
+        },
+    },
+    haveChildren: true,
+    editComponents: [],
+};
 
 const ColMaterial: IMaterial = {
     name: '栅格',
