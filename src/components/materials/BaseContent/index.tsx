1078e76f241686ddfa1a88921e7176f41c49246f
rywaroy <464485682@qq.com>
2020-08-16 15:25:06 +0800

    feat: style

--- src/components/materials/BaseContent/index.tsx
+++ src/components/materials/BaseContent/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import styles from './index.less';
 
 const BaseContent: React.FC<any> = (props) => (
-    <div {...props} />
+    <div className={styles.baseContent} {...props} />
 );
 
 export default BaseContent;
