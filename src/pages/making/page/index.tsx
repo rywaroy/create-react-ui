90b92925f3c1a25792e44f7b0a647376d564ec58
rywaroy <464485682@qq.com>
2020-08-16 15:26:22 +0800

    feat: 删除、移动、清空功能

--- src/pages/making/page/index.tsx
+++ src/pages/making/page/index.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { connect } from 'dva';
 import cloneDeep from 'lodash/cloneDeep';
 import { GlobalModelState } from '@/models/global';
-import MaterialList from '@/components/MaterialList';
 import materials, { baseContent } from '@/components/materials';
 import { IMaterial } from '@/types/making';
+import MaterialList from '../components/MaterialList';
+import MaterialContent from '../components/MaterialContent';
 import styles from './index.less';
 
 interface IProps {
@@ -30,9 +31,9 @@ class Making extends React.Component<IProps, IState> {
             materialList: [baseContent],
             material: null,
             id: 0,
-            codeVisible: false,
-            codeKey: Math.random(),
-            code: '',
+            // codeVisible: false,
+            // codeKey: Math.random(),
+            // code: '',
         };
     }
 
@@ -59,6 +60,7 @@ class Making extends React.Component<IProps, IState> {
             if (material.children) {
                 const children = [...material.children];
                 children.forEach((child) => {
+                    child = cloneDeep(child); // 防止子组件相同引用地址
                     child.active = false;
                     child.pid = key;
                     child.id = ++id;
@@ -71,6 +73,7 @@ class Making extends React.Component<IProps, IState> {
             if (material.children) {
                 let children = [...material.children];
                 children = children.map((child) => {
+                    child = cloneDeep(child); // 防止子组件相同引用地址
                     child.active = false;
                     child.pid = key;
                     child.id = ++id;
@@ -102,6 +105,68 @@ class Making extends React.Component<IProps, IState> {
         });
     };
 
+    // 删除物料组件
+    deleteMaterial = (cid: number) => {
+        let materials = cloneDeep(this.state.materialList);
+        const ids: number[] = [cid];
+        function findMaterial(pid: number) {
+        materials!.forEach((material) => {
+            if (material.pid === pid) {
+                ids.push(material.id);
+                findMaterial(material.id);
+            }
+        });
+        }
+        findMaterial(cid);
+        materials = materials.filter((item) => ids.indexOf(item.id) === -1);
+        this.setState({
+            material: null,
+            id: 0,
+            materialList: materials,
+        });
+    }
+
+    dorpMove = (cid: number, tid: number) => {
+        const materials = [...this.state.materialList];
+        const cIndex = this.findMaterialIndex(materials, cid);
+        const mc = materials.splice(cIndex, 1)[0];
+        const tIndex = this.findMaterialIndex(materials, tid);
+        const tc = materials[tIndex];
+        // 判断目标物料组件是否可以有子物料
+        if (tc.haveChildren) {
+            mc.pid = tc.id; // 改变pid
+            materials.push(mc); // 插入到最后一个
+        } else {
+            mc.pid = tc.pid; // 改变未目标pid
+            materials.splice(tIndex, 0, mc); // 在目标物料前插入
+        }
+        this.setState({
+            material: mc,
+            id: mc.id,
+            materialList: materials,
+        });
+    }
+
+    findMaterialIndex(materials: IMaterial[], id: number): number {
+        for (let i = 0; i < materials.length; i++) {
+            if (materials[i].id === id) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * 清空
+     */
+    clear = () => {
+        this.setState({
+            material: null,
+            id: 0,
+            materialList: [baseContent],
+        });
+    }
+
     componentDidMount() {
         // 收起菜单
         this.props.dispatch({
@@ -113,6 +178,7 @@ class Making extends React.Component<IProps, IState> {
     }
 
     render() {
+        const { materialList } = this.state;
         return (
             <div className={styles.pageWrap}>
                 <div className={styles.material}>
@@ -120,7 +186,15 @@ class Making extends React.Component<IProps, IState> {
                         materials={materials}
                         addMaterial={this.addMaterial} />
                 </div>
-                <div className={styles.pageContent} />
+                <div className={styles.pageContent}>
+                    <MaterialContent
+                        materialList={materialList}
+                        selectMaterial={this.selectMaterial}
+                        deleteMaterial={this.deleteMaterial}
+                        addMaterial={this.addMaterial}
+                        dorpMove={this.dorpMove}
+                        clear={this.clear} />
+                </div>
                 <div className={styles.edit} />
             </div>
         );
