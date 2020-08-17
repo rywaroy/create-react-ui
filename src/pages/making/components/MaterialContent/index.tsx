030b3b79bdf7c108474e9d99eb48cadb9a9a5ac8
rywaroy <464485682@qq.com>
2020-08-17 09:32:13 +0800

    refactor: 重构拖拽逻辑

--- src/pages/making/components/MaterialContent/index.tsx
+++ src/pages/making/components/MaterialContent/index.tsx
@@ -7,12 +7,10 @@ import MaterialBlock from '../MaterialBlock';
 import styles from './index.less';
 
 export interface IProps {
-  materialList: IMaterial[];
-  selectMaterial: (m: IMaterial) => void;
-  deleteMaterial: (id: number) => void;
-  addMaterial: (item: IMaterial, pid?: number, cid?: number) => void;
-  dorpMove: (cid: number, tid: number) => void;
-  clear: () => void;
+    materialList: IMaterial[];
+    setMaterialList: (materials: IMaterial[]) => void;
+    setMaterial: (material: IMaterial, id: number) => void;
+    clear: () => void;
 }
 
 export interface IState {
@@ -52,11 +50,137 @@ class MaterialContent extends React.Component<IProps, IState> {
     }
 
     dropAdd = (index: number, pid: number, id?: number) => {
-        this.props.addMaterial(materialComponents[index], pid, id);
+        this.addMaterial(materialComponents[index], pid, id);
     }
 
     create = () => {
 
+    }
+
+    /**
+     * 选中物料组件
+     */
+    selectMaterial = (material: IMaterial, materials?: IMaterial[]) => {
+        if (!materials) {
+            materials = [...this.props.materialList];
+        }
+        materials.forEach((item) => {
+            if (item.id === material.id) {
+                item.active = true;
+            } else {
+                item.active = false;
+            }
+        });
+        this.props.setMaterialList(materials);
+        this.props.setMaterial(material, material.id);
+    };
+
+    /**
+     * 添加物料组件
+     */
+    addMaterial = (item: IMaterial, pid?: number, cid?: number) => {
+        const material = cloneDeep(item);
+        const materials = cloneDeep(this.props.materialList);
+        let index = 0;
+        materials.forEach((item, i) => {
+            item.active = false;
+            if (item.id === cid) {
+                index = i;
+            }
+        });
+        const key = Math.random();
+        material.id = key;
+        material.active = true;
+        material.pid = pid || 1;
+        // 没有child id 表示添加物料组件
+        if (!cid) {
+            materials.push(material);
+            if (material.children) {
+                const children = [...material.children];
+                children.forEach((child) => {
+                    child = cloneDeep(child); // 防止子组件相同引用地址
+                    child.active = false;
+                    child.pid = key;
+                    child.id = Math.random();
+                    materials.push(child);
+                });
+                delete material.children;
+            }
+        } else { // 有child id 表示拖拽插入指定child id上方
+            materials.splice(index, 0, material);
+            if (material.children) {
+                let children = [...material.children];
+                children = children.map((child) => {
+                    child = cloneDeep(child); // 防止子组件相同引用地址
+                    child.active = false;
+                    child.pid = key;
+                    child.id = Math.random();
+                    return child;
+                });
+                materials.splice(index + 1, 0, ...children);
+                delete material.children;
+            }
+        }
+        this.selectMaterial(material, materials);
+    };
+
+    /**
+     * 删除物料组件
+     */
+    deleteMaterial = (cid: number) => {
+        let materials = cloneDeep(this.props.materialList);
+        const ids: number[] = [cid];
+        function findMaterial(pid: number) {
+        materials!.forEach((material) => {
+            if (material.pid === pid) {
+                ids.push(material.id);
+                findMaterial(material.id);
+            }
+        });
+        }
+        findMaterial(cid);
+        materials = materials.filter((item) => ids.indexOf(item.id) === -1);
+        this.props.setMaterialList(materials);
+        this.props.setMaterial(null, 0);
+    }
+
+    findMaterialIndex(materials: IMaterial[], id: number): number {
+        for (let i = 0; i < materials.length; i++) {
+            if (materials[i].id === id) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    dorpMove = (cid: number, tid: number) => {
+        const materials = [...this.props.materialList];
+        const cIndex = this.findMaterialIndex(materials, cid);
+        const mc = materials.splice(cIndex, 1)[0];
+        const tIndex = this.findMaterialIndex(materials, tid);
+        const tc = materials[tIndex];
+        // 判断目标物料组件是否可以有子物料
+        if (tc.haveChildren) {
+            mc.pid = tc.id; // 改变pid
+            materials.push(mc); // 插入到最后一个
+        } else {
+            mc.pid = tc.pid; // 改变未目标pid
+            materials.splice(tIndex, 0, mc); // 在目标物料前插入
+        }
+        this.props.setMaterialList(materials);
+        this.props.setMaterial(mc, mc.id);
+    }
+
+    /**
+     * 开始拖拽
+     */
+    dragStart = (id: number) => {
+        // const materials = [...this.state.materialList];
+        // const index = this.findMaterialIndex(materials, id);
+        // const material = materials.splice(index, 1)[0];
+        // this.setState({
+        //     materialList: materials,
+        // });
     }
 
     render() {
@@ -78,10 +202,11 @@ class MaterialContent extends React.Component<IProps, IState> {
                                 key={item.id}
                                 visual={visual}
                                 material={item}
-                                selectMaterial={(m) => this.props.selectMaterial(m)}
-                                deleteMaterial={(id) => this.props.deleteMaterial(id)}
+                                selectMaterial={(m) => this.selectMaterial(m)}
+                                deleteMaterial={(id) => this.deleteMaterial(id)}
                                 dropAdd={this.dropAdd}
-                                dorpMove={this.props.dorpMove}
+                                dorpMove={this.dorpMove}
+                                dragStart={this.dragStart}
                             />
                         ))
                     }
--- src/pages/making/page/index.tsx
+++ src/pages/making/page/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'dva';
-import cloneDeep from 'lodash/cloneDeep';
 import { GlobalModelState } from '@/models/global';
 import materials, { baseContent } from '@/components/materials';
 import { IMaterial } from '@/types/making';
@@ -22,8 +21,6 @@ interface IState {
     code: string;
 }
 
-let id = 1;
-
 class Making extends React.Component<IProps, IState> {
     constructor(props: IProps) {
         super(props);
@@ -37,124 +34,7 @@ class Making extends React.Component<IProps, IState> {
         };
     }
 
-    /**
-     * 添加物料组件
-     */
-    addMaterial = (item: IMaterial, pid?: number, cid?: number) => {
-        const material = cloneDeep(item);
-        const materials = cloneDeep(this.state.materialList);
-        let index = 0;
-        materials.forEach((item, i) => {
-            item.active = false;
-            if (item.id === cid) {
-                index = i;
-            }
-        });
-        const key = ++id;
-        material.id = key;
-        material.active = true;
-        material.pid = pid || 1;
-        // 没有child id 表示添加物料组件
-        if (!cid) {
-            materials.push(material);
-            if (material.children) {
-                const children = [...material.children];
-                children.forEach((child) => {
-                    child = cloneDeep(child); // 防止子组件相同引用地址
-                    child.active = false;
-                    child.pid = key;
-                    child.id = ++id;
-                    materials.push(child);
-                });
-                delete material.children;
-            }
-        } else { // 有child id 表示拖拽插入指定child id上方
-            materials.splice(index, 0, material);
-            if (material.children) {
-                let children = [...material.children];
-                children = children.map((child) => {
-                    child = cloneDeep(child); // 防止子组件相同引用地址
-                    child.active = false;
-                    child.pid = key;
-                    child.id = ++id;
-                    return child;
-                });
-                materials.splice(index + 1, 0, ...children);
-                delete material.children;
-            }
-        }
-        this.selectMaterial(material, materials);
-    };
-
-    // 选中物料组件
-    selectMaterial = (material: IMaterial, materials?: IMaterial[]) => {
-        if (!materials) {
-            materials = [...this.state.materialList];
-        }
-        materials.forEach((item) => {
-            if (item.id === material.id) {
-                item.active = true;
-            } else {
-                item.active = false;
-            }
-        });
-        this.setState({
-            material,
-            id: material.id,
-            materialList: materials,
-        });
-    };
-
-    // 删除物料组件
-    deleteMaterial = (cid: number) => {
-        let materials = cloneDeep(this.state.materialList);
-        const ids: number[] = [cid];
-        function findMaterial(pid: number) {
-        materials!.forEach((material) => {
-            if (material.pid === pid) {
-                ids.push(material.id);
-                findMaterial(material.id);
-            }
-        });
-        }
-        findMaterial(cid);
-        materials = materials.filter((item) => ids.indexOf(item.id) === -1);
-        this.setState({
-            material: null,
-            id: 0,
-            materialList: materials,
-        });
-    }
-
-    dorpMove = (cid: number, tid: number) => {
-        const materials = [...this.state.materialList];
-        const cIndex = this.findMaterialIndex(materials, cid);
-        const mc = materials.splice(cIndex, 1)[0];
-        const tIndex = this.findMaterialIndex(materials, tid);
-        const tc = materials[tIndex];
-        // 判断目标物料组件是否可以有子物料
-        if (tc.haveChildren) {
-            mc.pid = tc.id; // 改变pid
-            materials.push(mc); // 插入到最后一个
-        } else {
-            mc.pid = tc.pid; // 改变未目标pid
-            materials.splice(tIndex, 0, mc); // 在目标物料前插入
-        }
-        this.setState({
-            material: mc,
-            id: mc.id,
-            materialList: materials,
-        });
-    }
-
-    findMaterialIndex(materials: IMaterial[], id: number): number {
-        for (let i = 0; i < materials.length; i++) {
-            if (materials[i].id === id) {
-                return i;
-            }
-        }
-        return 0;
-    }
+    materialContent: MaterialContent;
 
     /**
      * 清空
@@ -165,6 +45,10 @@ class Making extends React.Component<IProps, IState> {
             id: 0,
             materialList: [baseContent],
         });
+    }
+
+    addMaterial = (material: IMaterial) => {
+        this.materialContent.addMaterial(material);
     }
 
     componentDidMount() {
@@ -188,11 +72,10 @@ class Making extends React.Component<IProps, IState> {
                 </div>
                 <div className={styles.pageContent}>
                     <MaterialContent
+                        ref={el => { this.materialContent = el; }}
                         materialList={materialList}
-                        selectMaterial={this.selectMaterial}
-                        deleteMaterial={this.deleteMaterial}
-                        addMaterial={this.addMaterial}
-                        dorpMove={this.dorpMove}
+                        setMaterialList={(materialList) => this.setState({ materialList })}
+                        setMaterial={(material, id) => this.setState({ material, id })}
                         clear={this.clear} />
                 </div>
                 <div className={styles.edit} />
