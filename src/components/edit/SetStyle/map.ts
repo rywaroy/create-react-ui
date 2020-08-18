0009045051e9ed82bfcd622e05619c3fb196411f
rywaroy <464485682@qq.com>
2020-08-18 14:36:25 +0800

    feat: 设置样式组件

---
+++ src/components/edit/SetStyle/map.ts
@@ -0,0 +1,20 @@
+interface IStyleMap {
+  [styleName: string]: string[];
+}
+
+export const styleMap: IStyleMap = {
+    width: [],
+    height: [],
+    backgroundColor: [],
+    margin: [],
+    marginLeft: [],
+    marginTop: [],
+    marginRight: [],
+    marginBottom: [],
+    padding: [],
+    paddingLeft: [],
+    paddingTop: [],
+    paddingRight: [],
+    paddingBottom: [],
+    position: ['absolute', 'relative', 'fixed'],
+};
