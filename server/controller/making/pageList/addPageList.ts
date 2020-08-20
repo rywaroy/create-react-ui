6b3b25c49e8a68a64aebb64d42dd1209f293dcf0
rywaroy <464485682@qq.com>
2020-08-20 10:29:22 +0800

    feat: 添加isPage 参数

--- server/controller/making/pageList/addPageList.ts
+++ server/controller/making/pageList/addPageList.ts
@@ -6,11 +6,12 @@ import IContext from '../../../types/context';
 interface IBody {
     title: string;
     value: any;
+    isPage: boolean;
 }
 
 export default function pageSave(ctx: IContext) {
     // @ts-ignore for travis
-    const { title, value }: IBody = ctx.request.body;
+    const { title, value, isPage }: IBody = ctx.request.body;
     const pagePath = path.join(process.cwd(), 'node_modules/.cache/crui/pageList.json');
     const pageExists = fs.existsSync(pagePath);
     if (pageExists) {
@@ -19,6 +20,7 @@ export default function pageSave(ctx: IContext) {
             title,
             id: uuidv4(),
             value,
+            isPage,
         });
         fs.writeJsonSync(pagePath, pageList);
         ctx.success(200, '保存成功');
