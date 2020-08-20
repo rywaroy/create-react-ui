7e3c69c599e6725f83a931d49edc64afbc3688d0
rywaroy <464485682@qq.com>
2020-08-20 09:37:43 +0800

    feat: 添加列表接口

---
+++ server/controller/making/pageList/addPageList.ts
@@ -0,0 +1,28 @@
+import path from 'path';
+import fs from 'fs-extra';
+import { v4 as uuidv4 } from 'uuid';
+import IContext from '../../../types/context';
+
+interface IBody {
+    title: string;
+    value: any;
+}
+
+export default function pageSave(ctx: IContext) {
+    // @ts-ignore for travis
+    const { title, value }: IBody = ctx.request.body;
+    const pagePath = path.join(process.cwd(), 'node_modules/.cache/crui/pageList.json');
+    const pageExists = fs.existsSync(pagePath);
+    if (pageExists) {
+        const pageList: any[] = fs.readJsonSync(pagePath);
+        pageList.push({
+            title,
+            id: uuidv4(),
+            value,
+        });
+        fs.writeJsonSync(pagePath, pageList);
+        ctx.success(200, '保存成功');
+    } else {
+        ctx.error(0, '保存失败');
+    }
+}
