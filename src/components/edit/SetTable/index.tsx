ea6475e27d97fb9c5a68a92e71271d94a18bd670
rywaroy <464485682@qq.com>
2020-08-24 10:37:21 +0800

    feat: 添加、编辑column

--- src/components/edit/SetTable/index.tsx
+++ src/components/edit/SetTable/index.tsx
@@ -2,17 +2,25 @@ import React, { useState, useEffect } from 'react';
 import { AutoComplete, Button } from 'antd';
 import { getLabelConfig } from '@/services/configlist';
 import { ILabelItem } from '@/types/configlist';
+import { IColumn } from '@/types/making';
+import SetColumn from './components/SetColumn';
 import styles from './index.less';
 
 interface IProps {
-    columns: any[];
+    columns: IColumn[];
     onChange: (values: any) => void;
 }
 
+const ACTION_KEY = 'action';
+
 const SetTable: React.FC<IProps> = (props) => {
     const columnList = [...props.columns];
-    const [columns, setColumns] = useState(columnList);
+    const [columns, setColumns] = useState<IColumn[]>(columnList);
+    const [column, setColumn] = useState<IColumn>({ title: '', dataIndex: '' });
+    const [index, setIndex] = useState<number | null>(0);
     const [label, setLabel] = useState<ILabelItem[]>([]);
+    const [visible, setVisible] = useState<boolean>(false);
+    const [key, setKey] = useState<number>(1);
 
     useEffect(() => {
         getLabelConfig()
@@ -69,12 +77,47 @@ const SetTable: React.FC<IProps> = (props) => {
         });
     };
 
+    /**
+     * 添加操作
+     */
+    const addOpt = () => {
+        setVisible(true);
+        setKey(Math.random());
+    };
+
+    /**
+     * 编辑column
+     */
+    const openColumn = (index: number) => {
+        setColumn(columns[index]);
+        setIndex(index);
+        setVisible(true);
+        setKey(Math.random());
+    };
+
+    /**
+     * 关闭编辑
+     */
+    const closeColumn = () => {
+        setVisible(false);
+    };
+
+    /**
+     * 编辑
+     */
+    const editColumn = (values: IColumn) => {
+        const list = [...columns];
+        list[index] = values;
+        setColumns(list);
+        closeColumn();
+    };
+
     return (
         <div>
             <div className={styles.tableTop}>
                 <div className={styles.tableTitle}>table属性</div>
                 <div>
-                    <Button type="primary" size="small" style={{ marginRight: '10px' }}>添加操作</Button>
+                    <Button type="primary" size="small" style={{ marginRight: '10px' }} onClick={() => addOpt()}>添加操作</Button>
                     <Button type="primary" icon="plus" size="small" onClick={() => addColumn()} />
                 </div>
             </div>
@@ -86,11 +129,18 @@ const SetTable: React.FC<IProps> = (props) => {
                             value={colum.title}
                             style={{ width: '150px', marginRight: '10px' }}
                             onChange={(value: string) => changeColumn(value, index)} />
+                        <Button type="primary" style={{ marginRight: '10px' }} icon="edit" size="small" onClick={() => openColumn(index)} />
                         <Button type="primary" icon="minus" size="small" onClick={() => deleteColumn(index)} />
                     </div>
                 ))
             }
             <Button type="primary" size="small" onClick={() => submit()}>提交</Button>
+            <SetColumn
+                visible={visible}
+                column={column}
+                key={key}
+                onCancel={closeColumn}
+                onOk={editColumn} />
         </div>
     );
 };
