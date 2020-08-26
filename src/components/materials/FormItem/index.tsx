f8302b83617ea8f9f66d00d1d6c59e89cbb193cb
rywaroy <464485682@qq.com>
2020-08-26 14:16:21 +0800

    feat: form item 物料组件

---
+++ src/components/materials/FormItem/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Form } from 'antd';
+import { IMaterial } from '@/types/making';
+
+const FormItem: React.FC<any> = (props) => (
+    <Form.Item {...props} />
+);
+
+export const FormItemMaterial: IMaterial = {
+    name: 'Form.Item',
+    tag: 'Form.Item',
+    from: 'antd',
+    id: Math.random(),
+    component: FormItem,
+    intro: 'Form Input标签',
+    props: {
+        label: '标签名',
+    },
+    haveChildren: false,
+    editComponents: [
+        { name: 'prop', props: { propName: 'label', propType: 'string' } },
+        { name: 'layout' },
+    ],
+};
+
+export default FormItem;
