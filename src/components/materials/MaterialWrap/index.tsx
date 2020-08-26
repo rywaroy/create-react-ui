091109e8ed36b053c8a97eeae77b8fc60e89e883
rywaroy <464485682@qq.com>
2020-08-26 19:23:54 +0800

    fix: 组件子元素

--- src/components/materials/MaterialWrap/index.tsx
+++ src/components/materials/MaterialWrap/index.tsx
@@ -5,19 +5,40 @@ export default function materialWrap(Component: React.FC | React.ComponentClass,
         const wrapProps = {};
         let materialProps = {};
         let defaultProps = {};
+        const materialChildren = [];
+        const wrapChildren = [];
         Object.keys(props).forEach(prop => {
             if (prop === 'props') {
                 materialProps = props[prop];
             } else if (prop === 'defaultprops') {
                 defaultProps = props[prop];
+            } else if (prop === 'children') {
+                props.children.forEach(item => {
+                    if (item) {
+                        if (item.props?.className === 'dragIcon' || item.props?.className === 'blockBottom') {
+                            wrapChildren.push(item);
+                        } else {
+                            materialChildren.push(item);
+                        }
+                    }
+                });
             } else {
                 wrapProps[prop] = props[prop];
             }
         });
         return (
             <div {...wrapProps} style={{ display }}>
-                <Component {...materialProps} {...defaultProps} />
-                {props.children}
+                {
+                    materialChildren.length > 0
+                        ? (
+                            <Component {...materialProps} {...defaultProps}>
+                                {materialChildren}
+                            </Component>
+                        )
+                        : <Component {...materialProps} {...defaultProps} />
+                }
+
+                {wrapChildren}
             </div>
         );
     };
