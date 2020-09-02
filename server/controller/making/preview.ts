c30d08705ee6cd2d8b2a1311b1a2c36a6b13fa81
rywaroy <464485682@qq.com>
2020-09-02 20:30:45 +0800

    feat: 预览接口

--- server/controller/making/preview.ts
+++ server/controller/making/preview.ts
@@ -1,5 +1,7 @@
 import IContext from '../../types/context';
 import { IMaterial } from '../../types/making';
+import Generator from './generator';
+import plugin from './plugin';
 
 interface IBody {
     materials: IMaterial[];
@@ -8,6 +10,10 @@ interface IBody {
 export default function preview(ctx: IContext) {
     // @ts-ignore for travis
     const { materials }: IBody = ctx.request.body;
-    console.log(materials);
-    ctx.success(200, '成功');
+    const generator = new Generator(materials, {
+        plugin,
+    });
+    ctx.success(200, '成功', {
+        code: generator.create(),
+    });
 }
