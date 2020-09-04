f76936179dc52a638a78fa9a2b0b22684635d471
rywaroy <464485682@qq.com>
2020-09-04 22:02:05 +0800

    feat

--- src/pages/making/components/PageProps/index.tsx
+++ src/pages/making/components/PageProps/index.tsx
@@ -22,59 +22,57 @@ class PageProps extends React.Component<IProps, IState> {
         const { folders } = this.props;
 
         return (
-            <div>
-                <Form>
-                    <Form.Item label="导出文件夹">
-                        {getFieldDecorator('url', {
+            <Form>
+                <Form.Item label="导出文件夹">
+                    {getFieldDecorator('url', {
+                        rules: [
+                            {
+                                required: true,
+                                message: '请填写导出文件夹',
+                            },
+                        ],
+                    })(
+                        <TreeSelect
+                            showSearch
+                            style={{ width: '100%' }}
+                            dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
+                            placeholder="请选择路径"
+                            allowClear
+                            treeData={folders}
+                        />,
+                    )}
+                </Form.Item>
+                <Form.Item label="页面名字">
+                    {getFieldDecorator('name', {
+                        rules: [
+                            {
+                                required: true,
+                                message: '请填写页面名字',
+                            },
+                            {
+                                pattern: /^((?!\\|\/|:|\?|\*|"|<|>|\^).)*$/,
+                                message: '请填写正确页面名字',
+                            },
+                        ],
+                    })(<Input placeholder="请填写页面名字" />)}
+                </Form.Item>
+                <Form.Item label="namespace">
+                    {
+                        getFieldDecorator('namespace', {
                             rules: [
                                 {
                                     required: true,
-                                    message: '请填写导出文件夹',
-                                },
-                            ],
-                        })(
-                            <TreeSelect
-                                showSearch
-                                style={{ width: '100%' }}
-                                dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
-                                placeholder="请选择路径"
-                                allowClear
-                                treeData={folders}
-                            />,
-                        )}
-                    </Form.Item>
-                    <Form.Item label="页面名字">
-                        {getFieldDecorator('name', {
-                            rules: [
-                                {
-                                    required: true,
-                                    message: '请填写页面名字',
+                                    message: '请填写页面空间命名',
                                 },
                                 {
-                                    pattern: /^((?!\\|\/|:|\?|\*|"|<|>|\^).)*$/,
-                                    message: '请填写正确页面名字',
+                                    pattern: /^[a-zA-Z$_][a-zA-Z\d_]*$/,
+                                    message: '请填写正确空间命名',
                                 },
                             ],
-                        })(<Input placeholder="请填写页面名字" />)}
-                    </Form.Item>
-                    <Form.Item label="namespace">
-                        {
-                            getFieldDecorator('namespace', {
-                                rules: [
-                                    {
-                                        required: true,
-                                        message: '请填写页面空间命名',
-                                    },
-                                    {
-                                        pattern: /^[a-zA-Z$_][a-zA-Z\d_]*$/,
-                                        message: '请填写正确空间命名',
-                                    },
-                                ],
-                            })(<Input placeholder="请输入model namespace" />)
-                        }
-                    </Form.Item>
-                </Form>
-            </div>
+                        })(<Input placeholder="请输入model namespace" />)
+                    }
+                </Form.Item>
+            </Form>
         );
     }
 }
