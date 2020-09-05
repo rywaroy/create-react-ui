b683b775d50756d41c9ce1a6479de317ea798cac
rywaroy <464485682@qq.com>
2020-09-05 21:10:50 +0800

    feat: 模板字符串解析插件

--- server/controller/making/plugin/index.ts
+++ server/controller/making/plugin/index.ts
@@ -1,7 +1,11 @@
 import children from './children';
 import table from './table';
+import page from './page';
+import template from './template';
 
 export default [
     children,
+    template,
     table,
+    page,
 ];
--- server/controller/making/plugin/template.ts
+++ server/controller/making/plugin/template.ts
@@ -5,10 +5,49 @@ import { IMaterial } from '../../../types/making';
 export default function template(material: IMaterial, generatorMaterial: GeneratorMaterial, generator: Generator) {
     Object.keys(material.props).forEach(key => {
         if (typeof material.props[key] === 'string') {
-            // @ts-ignore
-            with (generator) {
-                material.props[key] = material.props[key].replace(/{{(.*?)}}/g, ($1, $2) => eval($2));
-            }
+            material.props[key] = change(material.props[key]);
         }
     });
+    function deep(object) {
+        if (typeof object === 'string') {
+            return change(object);
+        } if (Array.isArray(object)) {
+            return object.map(item => deep(item));
+        }
+        Object.keys(object).forEach(key => {
+            object[key] = deep(object[key]);
+        });
+        return object;
+    }
+    if (material?.ext?.code) {
+        material.ext.code = deep(material.ext.code);
+    }
+
+    function change(str: string) {
+        if (!/{{(.*?)}}/.test(str)) {
+            return str;
+        }
+        // @ts-ignore
+        // with (generator) {
+        //     return str.replace(/{{(.*?)}}/g, ($1, $2) => eval($2));
+        // }
+        const args = [];
+        const fns = [];
+        for (const prop in generator) {
+            if (typeof generator[prop] === 'function') {
+                fns.push(prop);
+            } else {
+                args.push(prop);
+            }
+        }
+        let s: string;
+        const code = `
+            const { ${args} } = generator;
+            let { ${fns} } = generator;
+            ${fns.map(fn => `${fn} = ${fn}.bind(generator);`).join('\n')}
+            s = str.replace(/{{(.*?)}}/g, ($1, $2) => eval($2));
+        `;
+        eval(code);
+        return s;
+    }
 }
