a29bffb6b4028700bedd2a6a360cbacd1623bc69
rywaroy <464485682@qq.com>
2020-09-07 19:57:44 +0800

    fix: 判断children属性为string情况

--- server/controller/making/plugin/children.ts
+++ server/controller/making/plugin/children.ts
@@ -1,9 +1,12 @@
 import GeneratorMaterial from '../generator/GeneratorMaterial';
 import { IMaterial } from '../../../types/making';
 
+/**
+ * 处理children属性
+ */
 export default function children(material: IMaterial, generatorMaterial: GeneratorMaterial) {
     if (material.props.children) {
-        const child = material.props.children[0];
+        const child = typeof material.props.children === 'string' ? material.props.children : material.props.children[0];
         delete material.props.children;
         generatorMaterial.on('after-create-startTag', () => {
             generatorMaterial.jsx += child;
