952b2512ff6fe2ddb1ef33a5923ada61af1ea29c
rywaroy <464485682@qq.com>
2020-09-08 19:51:14 +0800

    feat: tabel插件添加操作栏处理

--- server/controller/making/plugin/table.ts
+++ server/controller/making/plugin/table.ts
@@ -6,6 +6,19 @@ import { IMaterial } from '../../../types/making';
 export default function table(material: IMaterial) {
     if (material.tag === 'Table') {
         delete material.props.dataSource;
+        const last = material.props.columns[material.props.columns.length - 1];
+        if (last.key === 'action') {
+            last.render = `() => (
+            <>
+                ${last.opts.map((item) => (item.link
+        ? `<a href="/" target="_blank" className="mr10">${item.text}</a>`
+        : `<span className="opt-link">${item.text}</span>`
+    )).join('\n')}
+            </>
+            )`;
+            delete last.opts;
+            material.copyProps.columns = material.props.columns;
+        }
         delete material.props.columns;
         material.props.columnsFS = 'columns()';
     }
