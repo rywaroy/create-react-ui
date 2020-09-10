8d56d9432f9a63ec9d756859e5e40d22a4632fad
rywaroy <464485682@qq.com>
2020-09-10 16:52:08 +0800

    feat: create接口

--- server/controller/making/index.ts
+++ server/controller/making/index.ts
@@ -2,10 +2,12 @@ import pageList from './pageList';
 import addPageList from './addPageList';
 import delPageList from './delPageList';
 import preview from './preview';
+import create from './create';
 
 export default {
     pageList,
     addPageList,
     delPageList,
     preview,
+    create,
 };
