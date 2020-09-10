55cdd9ddd98a1dcbddd5fbd36887adc284e24b8a
rywaroy <464485682@qq.com>
2020-09-10 16:52:27 +0800

    style

--- server/controller/making/plugin/table.ts
+++ server/controller/making/plugin/table.ts
@@ -8,14 +8,9 @@ export default function table(material: IMaterial) {
         delete material.props.dataSource;
         const last = material.props.columns[material.props.columns.length - 1];
         if (last.key === 'action') {
-            last.render = `() => (
-            <>
-                ${last.opts.map((item) => (item.link
-        ? `<a href="/" target="_blank" className="mr10">${item.text}</a>`
-        : `<span className="opt-link">${item.text}</span>`
-    )).join('\n')}
-            </>
-            )`;
+            last.render = `() => (<>${last.opts.map((item) => (item.link
+                ? `<a href="/" target="_blank" className="mr10">${item.text}</a>`
+                : `<span className="opt-link">${item.text}</span>`)).join('')}</>)`;
             delete last.opts;
             material.copyProps.columns = material.props.columns;
         }
