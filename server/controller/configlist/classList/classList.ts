9c623fe54f08504aa1c87106b3f60be842d08961
rywaroy <464485682@qq.com>
2020-09-11 11:12:40 +0800

    feat: 添加display参数

--- server/controller/configlist/classList/classList.ts
+++ server/controller/configlist/classList/classList.ts
@@ -1,12 +1,17 @@
 import path from 'path';
 import fs from 'fs-extra';
 import IContext from '../../../types/context';
+import { IClassItem } from '../../../types/configlist';
 
 export default async function classList(ctx: IContext) {
+    const { display } = ctx.query;
     const classPath = path.join(process.cwd(), 'node_modules/.cache/crui/classList.json');
     const classExists = fs.existsSync(classPath);
     if (classExists) {
-        const classList = fs.readJsonSync(classPath);
+        let classList = fs.readJsonSync(classPath);
+        if (display) {
+            classList = classList.filter((item: IClassItem) => item.display === Boolean(display));
+        }
         ctx.success(200, '获取成功', classList);
     } else {
         fs.ensureFileSync(classPath);
