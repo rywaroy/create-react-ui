b8541506ec75c17ca6c52d586507ebe25ffe20e5
rywaroy <464485682@qq.com>
2020-09-15 11:13:38 +0800

    feat: 添加modal组件插件

--- server/controller/making/plugin/generateModal.ts
+++ server/controller/making/plugin/generateModal.ts
@@ -4,7 +4,7 @@ import { IMaterial } from '../../../types/making';
  * 处理GenerateModal组件
  */
 export default function generateModal(material: IMaterial) {
-    if (material.tag === 'GenerateModal') {
+    if (material.tag === 'GenerateModal' || material.tag === 'Modal') {
         delete material.props.visible;
         delete material.props.modalName;
         delete material.props.modalForm;
