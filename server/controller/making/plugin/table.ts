4293be0c442264b2926c86c816b595a26438b554
rywaroy <464485682@qq.com>
2020-09-20 22:39:47 +0800

    feat: 按钮弹窗绑定优化，添加record

--- server/controller/making/plugin/table.ts
+++ server/controller/making/plugin/table.ts
@@ -9,9 +9,9 @@ export default function table(material: IMaterial) {
         const last = material.props.columns[material.props.columns.length - 1];
         let methods = [];
         if (last.key === 'action') {
-            last.render = `() => (<>${last.opts.map((item: IOpt) => (item.link
+            last.render = `(record) => (<>${last.opts.map((item: IOpt) => (item.link
                 ? `<a href="/" target="_blank" className="mr10">${item.text}</a>`
-                : `<span className="opt-link"${item.linkModal ? ` onClick={${item.linkModal}Open}` : ''}>${item.text}</span>`)).join('')}</>)`;
+                : `<span className="opt-link"${item.linkModal ? ` onClick={() => ${item.linkModal}Open(record)}` : ''}>${item.text}</span>`)).join('')}</>)`;
             methods = last.opts.filter((item: IOpt) => item.linkModal).map((item: IOpt) => `${item.linkModal}Open`);
             material.ext.modalMethods = methods;
             delete last.opts;
