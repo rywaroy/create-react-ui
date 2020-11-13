da251a8da05687df39a2c27fd29993df498e529a
rywaroy <464485682@qq.com>
2020-11-13 23:35:56 +0800

    chore: 陆运通table render模板

--- server/controller/making/plugin/table.ts
+++ server/controller/making/plugin/table.ts
@@ -9,9 +9,14 @@ export default function table(material: IMaterial) {
         const last = material.props.columns[material.props.columns.length - 1];
         let methods = [];
         if (last.key === 'action') {
-            last.render = `(record) => (<>${last.opts.map((item: IOpt) => (item.link
-                ? `<a href="/" target="_blank" className="mr10">${item.text}</a>`
-                : `<span className="opt-link"${item.linkModal ? ` onClick={() => ${item.linkModal}Open(record)}` : ''}>${item.text}</span>`)).join('')}</>)`;
+            if (material.project === '陆运通后台') {
+                last.render = `(record) => (<TableOpt>${last.opts.map((item: IOpt) => (
+                    `<TableOpt.Item${item.linkModal ? ` onClick={() => ${item.linkModal}Open(record)}` : ''} ${item.link ? 'type="link" target="_blank"' : ''}>${item.text}</TableOpt.Item>`))}</TableOpt>)`;
+            } else {
+                last.render = `(record) => (<>${last.opts.map((item: IOpt) => (item.link
+                    ? `<a href="/" target="_blank" className="mr10">${item.text}</a>`
+                    : `<span className="opt-link"${item.linkModal ? ` onClick={() => ${item.linkModal}Open(record)}` : ''}>${item.text}</span>`)).join('')}</>)`;
+            }
             methods = last.opts.filter((item: IOpt) => item.linkModal).map((item: IOpt) => `${item.linkModal}Open`);
             material.ext.modalMethods = methods;
             delete last.opts;
