afa5dbbf87615950182decd9bdd3ce5e8315c9fa
rywaroy <464485682@qq.com>
2020-11-18 11:28:00 +0800

    feat: 修改ListFilter组件模板，支持hooks

--- src/components/materials/LytListFilter/index.tsx
+++ src/components/materials/LytListFilter/index.tsx
@@ -344,6 +344,7 @@ export const LytListFilter = {
         refFS: 'formRef',
         onSearchFS: 'submit',
         onResetFS: 'reset',
+        formDataFS: 'formData',
     },
     haveChildren: false,
     editComponents: [
@@ -369,6 +370,8 @@ export const LytListFilter = {
                 },
                 variableDeclarator: [
                     'const formRef = useRef(null);',
+                    `const [{{namespace}}] = store;
+                     const { formData } = {{namespace}};`,
                 ],
             },
             'map.js': {
