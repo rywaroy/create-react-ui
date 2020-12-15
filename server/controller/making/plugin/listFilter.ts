4c37154e382eae81f6431128ceb1eac443dd6f43
rywaroy <464485682@qq.com>
2020-12-15 16:54:14 +0800

    feat: 删除模板代码中的引用和导出

--- server/controller/making/plugin/listFilter.ts
+++ server/controller/making/plugin/listFilter.ts
@@ -1,4 +1,4 @@
-import { IMaterial } from '../../../types/making';
+import { IMaterial, IPageOtion } from '../../../types/making';
 
 /**
  * 处理filters组件
@@ -9,13 +9,37 @@ export default function listFilter(material: IMaterial) {
             material.props.filtersFS = 'listFilters()';
             delete material.props.filters;
         }
+        const deleteProps = [];
         if (material.props.formSet && material.props.formSet.length > 0) {
             material.props.formSetFS = 'listFilters()';
+        } else {
+            deleteProps.push('listFilters');
         }
         delete material.props.formSet;
         if (material.props.otherSet && material.props.otherSet.length > 0) {
             material.props.otherSetFS = 'otherSet()';
+        } else {
+            deleteProps.push('otherSet');
         }
         delete material.props.otherSet;
+
+        // 删除模板代码中的引用和导出
+        if (deleteProps.length > 0) {
+            const exportList = material.ext.code['index.js'].importDeclaration['./map'].export;
+            for (let i = exportList.length - 1; i >= 0; i--) {
+                if (deleteProps.indexOf(exportList[i]) > -1) {
+                    exportList.splice(i, 1);
+                }
+            }
+
+            const codeList = (material.ext.code['map.js'] as IPageOtion).codes;
+            for (let i = codeList.length - 1; i >= 0; i--) {
+                for (let j = 0; j < deleteProps.length; j++) {
+                    if (codeList[i].indexOf(deleteProps[j]) > -1) {
+                        codeList.splice(i, 1);
+                    }
+                }
+            }
+        }
     }
 }
