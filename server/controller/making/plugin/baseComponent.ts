c43c7c0d4e82648dcb1388c925bf0d7929313bcd
rywaroy <464485682@qq.com>
2020-12-15 18:43:00 +0800

    chore: 模板删除 redux相关内容

--- server/controller/making/plugin/baseComponent.ts
+++ server/controller/making/plugin/baseComponent.ts
@@ -5,24 +5,24 @@ import { IMaterial, IComponentOption } from '../../../types/making';
  */
 export default function baseComponent(material: IMaterial) {
     if (material.id === 1) { // 根组件
-        if (material.props.project === '油涟后台') {
-            const index = (material.ext.code['index.js'] as IComponentOption);
-            index.importDeclaration.react.export = ['useEffect'];
-            index.importDeclaration.dva = {
-                export: ['useDispatch', 'useSelector'],
-            };
-            index.variableDeclarator = [
-                'const dispatch = useDispatch();',
-                'const {{namespace}} = useSelector(state => state.{{namespace}});',
-            ];
-            index.useEffect = [
-                `useEffect(() => {
-                    return () => {
-                        dispatch({ type: '{{namespace}}/resetState' });
-                    };
-                }, []);`,
-            ];
-        }
+        // if (material.props.project === '油涟后台') {
+        //     const index = (material.ext.code['index.js'] as IComponentOption);
+        //     index.importDeclaration.react.export = ['useEffect'];
+        //     index.importDeclaration.dva = {
+        //         export: ['useDispatch', 'useSelector'],
+        //     };
+        //     index.variableDeclarator = [
+        //         'const dispatch = useDispatch();',
+        //         'const {{namespace}} = useSelector(state => state.{{namespace}});',
+        //     ];
+        //     index.useEffect = [
+        //         `useEffect(() => {
+        //             return () => {
+        //                 dispatch({ type: '{{namespace}}/resetState' });
+        //             };
+        //         }, []);`,
+        //     ];
+        // }
 
         delete material.props.project;
     }
