e028e4c6f0494aea79b097819e22079863dd4a05
rywaroy <464485682@qq.com>
2020-12-15 17:46:32 +0800

    feat: 判断删玩情况

--- server/controller/making/plugin/listFilter.ts
+++ server/controller/making/plugin/listFilter.ts
@@ -24,21 +24,30 @@ export default function listFilter(material: IMaterial) {
         delete material.props.otherSet;
 
         // 删除模板代码中的引用和导出
-        if (deleteProps.length > 0) {
+        if (deleteProps.length > 0 && material.project === '陆运通后台') {
             const exportList = material.ext.code['index.js'].importDeclaration['./map'].export;
             for (let i = exportList.length - 1; i >= 0; i--) {
                 if (deleteProps.indexOf(exportList[i]) > -1) {
                     exportList.splice(i, 1);
                 }
             }
+            // 删完的情况
+            if (exportList.length === 0) {
+                delete material.ext.code['index.js'].importDeclaration['./map'];
+            }
 
             const codeList = (material.ext.code['map.js'] as IPageOtion).codes;
             for (let i = codeList.length - 1; i >= 0; i--) {
                 for (let j = 0; j < deleteProps.length; j++) {
-                    if (codeList[i].indexOf(deleteProps[j]) > -1) {
-                        codeList.splice(i, 1);
+                    if (codeList[i]) {
+                        if (codeList[i].indexOf(deleteProps[j]) > -1) {
+                            codeList.splice(i, 1);
+                        }
                     }
                 }
+            }
+            if (codeList.length === 0) {
+                delete material.ext.code['map.js'];
             }
         }
     }
