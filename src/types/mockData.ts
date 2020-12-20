85575f7e1e368b741dd0eb18d69ca3d0a17b8e54
rywaroy <464485682@qq.com>
2020-12-20 21:03:51 +0800

    feat: mock object interface

--- src/types/mockData.ts
+++ src/types/mockData.ts
@@ -1,10 +1,20 @@
 export interface mockData {
-  label: string;
-  labelMin?: number;
-  labelMax?: number;
-  value: string | number;
-  objectValue?: mockData[];
-  arrayValue?: mockData[][];
-  id: number;
-  pid: number;
+    label: string;
+    labelMin?: number;
+    labelMax?: number;
+    value: string | number;
+    objectValue?: mockData[];
+    arrayValue?: mockData[][];
+    id: number;
+    pid: number;
+}
+
+export interface mockObject {
+    url: string;
+    baseUrl?: string;
+    methods: 'GET' | 'POST';
+    path: string;
+    fileName?: string;
+    serverName?: string;
+    serverPath?: string;
 }
