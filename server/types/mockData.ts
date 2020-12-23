5436b9068724239768e3f1195ab038451ca7bdde
rywaroy <464485682@qq.com>
2020-12-23 23:28:52 +0800

    feat: mockDataParams interface

--- server/types/mockData.ts
+++ server/types/mockData.ts
@@ -1,9 +1,16 @@
 export interface mockObject {
+    /**API url */
     url: string;
+    /**项目的base URL */
     baseUrl?: string;
-    methods: 'GET' | 'POST';
+    /**请求 Method */
+    method: 'GET' | 'POST';
+    /**Mock 文件夹/文件路径 */
     path: string;
+    /**mock 文件名 */
     fileName?: string;
+    /**请求函数名 */
     serverName?: string;
+    /**请求函数文件路径 */
     serverPath?: string;
 }
--- src/types/mockData.ts
+++ src/types/mockData.ts
@@ -10,11 +10,22 @@ export interface mockData {
 }
 
 export interface mockObject {
+    /**API url */
     url: string;
+    /**项目的base URL */
     baseUrl?: string;
-    methods: 'GET' | 'POST';
+    /**请求 Method */
+    method: 'GET' | 'POST';
+    /**Mock 文件夹/文件路径 */
     path: string;
+    /**mock 文件名 */
     fileName?: string;
+    /**请求函数名 */
     serverName?: string;
+    /**请求函数文件路径 */
     serverPath?: string;
+}
+
+export interface mockDataParams extends mockObject {
+    mockObject: Object
 }
