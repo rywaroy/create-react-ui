d6a670c4ee65825fe2b456359d857e7f3487dd55
rywaroy <464485682@qq.com>
2020-12-23 23:30:37 +0800

    feat: 请求生成mock

--- src/pages/mockData/hooks/useCreateMock.ts
+++ src/pages/mockData/hooks/useCreateMock.ts
@@ -1,10 +1,24 @@
 import { useModal } from 'behooks';
+import { message } from 'antd';
+import { createMock } from '@/services/mockData';
+import { mockObject } from '@/types/mockData';
 
-export default function useCreateMock() {
+export default function useCreateMock(object: Object) {
     const { toggle, modalProps } = useModal();
+
+    const onCreate = (values: mockObject) => {
+        createMock({
+            mockObject: object,
+            ...values,
+        }).then(() => {
+            message.success('创建成功');
+            toggle();
+        });
+    };
 
     return {
         toggle,
         modalProps,
+        onCreate,
     };
 }
